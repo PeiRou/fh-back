6ba8b8a6f8c2cf854d50f3cf2fa92074d54807ff
laomumeme <laomumeme@gmail.com>
2018-06-17 21:23:11 +0800

    Initial commit

--- resources/views/back/log/login.blade.php
+++ resources/views/back/log/login.blade.php
@@ -9,7 +9,37 @@
         </div>
     </div>
     <div class="table-content">
-
+        <div class="table-quick-bar">
+            <div class="ui mini form">
+                <div class="fields">
+                    <div class="two wide field">
+                        <input type="text" id="account" placeholder="开始时间">
+                    </div>
+                    <div style="line-height: 32px;">-</div>
+                    <div class="one wide field">
+                        <input type="text" id="mobile" placeholder="结束时间">
+                    </div>
+                    <div class="one wide field">
+                        <input type="text" id="qq" placeholder="用户账户">
+                    </div>
+                    <div class="one wide field">
+                        <input type="text" id="minMoney" placeholder="登录IP">
+                    </div>
+                    <div class="one wide field">
+                        <input type="text" id="maxMoney" placeholder="域名">
+                    </div>
+                    <div class="one wide field">
+                        <input type="text" id="promoter" placeholder="IP信息">
+                    </div>
+                    <div class="one wide field">
+                        <button id="btn_search" class="fluid ui mini labeled icon teal button"><i class="search icon"></i> 查询 </button>
+                    </div>
+                    <div class="one wide field">
+                        <button id="reset" class="fluid ui mini labeled icon button"><i class="undo icon"></i> 重置 </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
