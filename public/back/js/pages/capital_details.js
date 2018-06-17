28a0b068e3f82d2fea102fc2e4bd334af42d899d
laomumeme <laomumeme@gmail.com>
2018-06-17 20:51:24 +0800

    Initial commit

--- public/back/js/pages/capital_details.js
+++ public/back/js/pages/capital_details.js
@@ -5,44 +5,48 @@ $(function () {
 
     $('#btn_search').on('click',function () {
         var type = $('#type').val();
-        typeTable(type);
+        var search = typeTable(type);
 
-        datatables = $('#capitalDetailsTable').DataTable({
-            searching: false,
-            bLengthChange: false,
-            processing: true,
-            serverSide: true,
-            ordering: false,
-            ajax: {
-                url:'/back/datatables/capitalDetails',
-                data:function (d) {
-                    d.type = $('#type').val();
+        if(search == true){
+            datatables = $('#capitalDetailsTable').DataTable({
+                searching: false,
+                bLengthChange: false,
+                processing: true,
+                serverSide: true,
+                ordering: false,
+                ajax: {
+                    url:'/back/datatables/capitalDetails',
+                    data:function (d) {
+                        d.type = $('#type').val();
+                    }
+                },
+                columns: [
+                    {}
+                ],
+                language: {
+                    "zeroRecords": "暂无数据",
+                    "info": "当前显示第 _PAGE_ 页，共 _PAGES_ 页",
+                    "infoEmpty": "没有记录",
+                    "loadingRecords": "请稍后...",
+                    "processing":     "读取中...",
+                    "paginate": {
+                        "first":      "首页",
+                        "last":       "尾页",
+                        "next":       "下一页",
+                        "previous":   "上一页"
+                    }
                 }
-            },
-            columns: [
-                {}
-            ],
-            language: {
-                "zeroRecords": "暂无数据",
-                "info": "当前显示第 _PAGE_ 页，共 _PAGES_ 页",
-                "infoEmpty": "没有记录",
-                "loadingRecords": "请稍后...",
-                "processing":     "读取中...",
-                "paginate": {
-                    "first":      "首页",
-                    "last":       "尾页",
-                    "next":       "下一页",
-                    "previous":   "上一页"
-                }
-            }
-        });
+            });
+        }
+
     })
 });
 
 function typeTable(type) {
+    var search = true;
     if(type == ""){
         alert('没有选择类型');
-        return false;
+        return search;
     }
     if(type == 't01'){
         var th = '<th>用户</th>\n' +
@@ -65,4 +69,5 @@ function typeTable(type) {
             '<th>交易金额</th>\n';
     }
     $('#capitalDetailsTable thead tr').html(th);
+    return search;
 }
