85a9d0cf5e9afda81876b50251cad04b373b5941
laomumeme <laomumeme@gmail.com>
2018-06-17 20:27:27 +0800

    Initial commit

--- public/back/js/pages/capital_details.js
+++ public/back/js/pages/capital_details.js
@@ -3,28 +3,28 @@ $(function () {
     $('#menu-financeManage').addClass('nav-show');
     $('#menu-financeManage-capitalDetails').addClass('active');
 
-    $('#capitalDetailsTable').DataTable({
-        searching: false,
-        bLengthChange: false,
-        processing: true,
-        serverSide: true,
-        ordering: false,
-        ajax: '/back/datatables/capitalDetails',
-        columns: [
-            {}
-        ],
-        language: {
-            "zeroRecords": "暂无数据",
-            "info": "当前显示第 _PAGE_ 页，共 _PAGES_ 页",
-            "infoEmpty": "没有记录",
-            "loadingRecords": "请稍后...",
-            "processing":     "读取中...",
-            "paginate": {
-                "first":      "首页",
-                "last":       "尾页",
-                "next":       "下一页",
-                "previous":   "上一页"
-            }
-        }
-    });
+    // $('#capitalDetailsTable').DataTable({
+    //     searching: false,
+    //     bLengthChange: false,
+    //     processing: true,
+    //     serverSide: true,
+    //     ordering: false,
+    //     ajax: '/back/datatables/capitalDetails',
+    //     columns: [
+    //         {}
+    //     ],
+    //     language: {
+    //         "zeroRecords": "暂无数据",
+    //         "info": "当前显示第 _PAGE_ 页，共 _PAGES_ 页",
+    //         "infoEmpty": "没有记录",
+    //         "loadingRecords": "请稍后...",
+    //         "processing":     "读取中...",
+    //         "paginate": {
+    //             "first":      "首页",
+    //             "last":       "尾页",
+    //             "next":       "下一页",
+    //             "previous":   "上一页"
+    //         }
+    //     }
+    // });
 });
