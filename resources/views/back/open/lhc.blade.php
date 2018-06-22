60966a602bf9162a9c13ba7036c8dc4dd6b37115
vincent-lee <laomumeme@gmail.com>
2018-06-22 23:50:36 +0800

    Initial commit

--- resources/views/back/open/lhc.blade.php
+++ resources/views/back/open/lhc.blade.php
@@ -3,5 +3,12 @@
 @section('title','历史开奖-六合彩')
 
 @section('content')
-
+    <div class="content-top">
+        <div class="breadcrumb">
+            <b>位置：</b>六合彩
+        </div>
+        <div class="content-top-buttons">
+            <span onclick="addBank()">1</span>
+        </div>
+    </div>
 @endsection
