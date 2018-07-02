d56a55afa1cdeac21c1fb992d4d81d2fea8d5b85
vincent-lee <laomumeme@gmail.com>
2018-07-02 18:03:17 +0800

    1

--- app/Http/Controllers/Back/SrcViewController.php
+++ app/Http/Controllers/Back/SrcViewController.php
@@ -291,10 +291,28 @@ class SrcViewController extends Controller
     public function payOnlineDateChange($date = "")
     {
         $dt = Carbon::now();
+        //本周
+        //当前日期
+        $sdefaultDate = date("Y-m-d");
+        //$first =1 表示每周星期一为开始日期 0表示每周日为开始日期
+        $first=1;
+        //获取当前周的第几天 周日是 0 周一到周六是 1 - 6
+        $w=date('w',strtotime($sdefaultDate));
+        //获取本周开始日期，如果$w是0，则表示周日，减去 6 天
+        $week_start = date('Y-m-d',strtotime("$sdefaultDate -".($w ? $w - $first : 6).' days'));
+        //本周结束日期
+        $week_end = date('Y-m-d',strtotime("$week_start +6 days"));
+
         if($date == 'yesterday'){
             return response()->json([
                 'start'=> $dt->yesterday()->toDateString(),
                 'end' => $dt->yesterday()->toDateString()
+            ]);
+        }
+        if($date == 'week'){
+            return response()->json([
+                'start'=> $week_start,
+                'end' => $week_end
             ]);
         }
     }
