e79770dfb9ca45a48e01a5694fcc9e721c3b5858
vincent-lee <laomumeme@gmail.com>
2018-07-03 15:13:40 +0800

    1

--- public/back/js/pages/recharge_record.js
+++ public/back/js/pages/recharge_record.js
@@ -70,7 +70,7 @@ $(function () {
                 d.fullName = $('#fullName').val();
                 d.startTime = $('#startTime').val();
                 d.endTime = $('#endTime').val();
-                d.killTestUser = $('#killTestUser').val();
+                d.killTestUser = $('#killTestUser:checked').val();
             }
         },
         columns: [
