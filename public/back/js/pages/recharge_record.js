09daaaf788e277d90bffefe89796e6aeb7abaf36
vincent-lee <laomumeme@gmail.com>
2018-07-03 15:10:01 +0800

    1

--- public/back/js/pages/recharge_record.js
+++ public/back/js/pages/recharge_record.js
@@ -70,7 +70,7 @@ $(function () {
                 d.fullName = $('#fullName').val();
                 d.startTime = $('#startTime').val();
                 d.endTime = $('#endTime').val();
-                d.killTestUser = $("input[type='checkbox']").attr('value');
+                d.killTestUser = $('#killTestUser').val();
             }
         },
         columns: [
--- resources/views/back/rechargeRecord.blade.php
+++ resources/views/back/rechargeRecord.blade.php
@@ -9,7 +9,7 @@
         </div>
         <div class="select-test-user">
             <label>
-                <input type="checkbox" value="1" id="killTestUser" checked>
+                <input type="checkbox" value="1" id="killTestUser" checked="checked" >
                 过滤测试用户
             </label>
         </div>
