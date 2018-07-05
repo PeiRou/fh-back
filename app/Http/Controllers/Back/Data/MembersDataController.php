9b87cb21a6167f6876d3fe70bb6fab114a736f49
vincent-lee <laomumeme@gmail.com>
2018-07-05 20:27:56 +0800

    1

--- app/Http/Controllers/Back/Data/MembersDataController.php
+++ app/Http/Controllers/Back/Data/MembersDataController.php
@@ -325,13 +325,13 @@ class MembersDataController extends Controller
             ->where('users.testFlag','!=',1)->orderBy('users.created_at','desc')->get();
         return DataTables::of($users)
             ->editColumn('online',function ($users) {
-                $key = 'user:'.md5($users->uid);
-                Redis::select(2);
-                if(Redis::exists($key)){
-                    return "<span class='on-line-point'></span>";
-                } else {
-                    return "<span class='off-line-point'></span>";
-                }
+//                $key = 'user:'.md5($users->uid);
+//                Redis::select(2);
+//                if(Redis::exists($key)){
+//                    return "<span class='on-line-point'></span>";
+//                } else {
+//                    return "<span class='off-line-point'></span>";
+//                }
 //                $userLastTime = strtotime($users->updated_at);
 //                $time = $now-$userLastTime;
 //                if($time > 3600)
