0a65c84c9115905e7e09d9fc116512be231240f7
vincent-lee <laomumeme@gmail.com>
2018-07-05 21:28:58 +0800

    1

--- app/Http/Controllers/Back/Data/BetDataController.php
+++ app/Http/Controllers/Back/Data/BetDataController.php
@@ -413,57 +413,60 @@ class BetDataController extends Controller
         $userId = $request->get('userId');
         $user = DB::table('users')->where('id',$userId)->first();
 //        return count($games);
-        $bet = Bets::select()
+        $bet = DB::table('bet')
+            ->leftJoin('game','bet.game_id','=','game.game_id')
+            ->leftJoin('play_cate','bet.game_id','=','play_cate.gameId')
+            ->leftJoin('play','bet.game_id','=','play.gameId')
+            ->select('bet.order_id as bet_order_id','game.game_name as g_game_name','bet.color as bet_color','bet.issue as bet_issue','bet.game_id as bet_game_id','bet.playcate_id as bet_playcate_id','bet.play_id as bet_play_id','bet.bet_money as bet_bet_money','bet.bunko as bet_bunko')
             ->where(function ($query) use ($games){
                 if(count($games) !== 0){
                     foreach ($games as $item){
-                        $query->orWhere("game_id",$item);
+                        $query->orWhere("bet.game_id",$item);
                     }
                 }
             })
-            ->where('user_id',$userId)->orderBy('created_at','desc')->get();
+            ->where('bet.user_id',$userId)->orderBy('bet.created_at','desc')->get();
         return DataTables::of($bet)
             ->editColumn('order_id',function ($bet){
-                return '<span>'.$bet->order_id.'</span>';
+                return '<span>'.$bet->bet_order_id.'</span>';
             })
             ->editColumn('user',function ($bet) use ($user){
                 return '<span>'.$user->username.'</span>';
             })
             ->editColumn('game',function ($bet){
-                $game = DB::table('game')->where('game_id',$bet->game_id)->first();
-                return '<span>'.$game->game_name.'</span>';
+                return '<span>'.$bet->g_game_name.'</span>';
             })
             ->editColumn('issue',function ($bet){
-                return '<span style="color: #'.$bet->color.'">'.$bet->issue.'</span>';
+                return '<span style="color: #'.$bet->bet_color.'">'.$bet->bet_issue.'</span>';
             })
             ->editColumn('play',function ($bet){
-                if($bet->game_id == 1){
-                    $weishu = strlen((int)$bet->game_id.$bet->playcate_id);
-                    $playid_weishu = strlen((int)$bet->play_id);
-                    $playCate = $bet->playcate_id;
-                    $play = substr($bet->play_id,$weishu,$playid_weishu-$weishu);
-                }
-                if($bet->game_id == 80 || $bet->game_id == 81 || $bet->game_id == 82 || $bet->game_id == 99 || $bet->game_id == 90 || $bet->game_id == 91){
-                    $playCate = substr($bet->play_id,2,3);
-                    $play = substr($bet->play_id,5,4);
-                }
-                if($bet->game_id == 50){
-                    $playCate = substr($bet->play_id,2,2);
-                    $play = substr($bet->play_id,4,3);
-                }
-                if($bet->game_id == 66 || $bet->game_id == 65){
-                    $playCate = substr($bet->play_id,2,2);
-                    $play = substr($bet->play_id,4,4);
-                }
-                $cate_txt = PlayCates::where('gameId',$bet->game_id)->where('id',$playCate)->first();
-                $play_txt = Play::where('gameId',$bet->game_id)->where('id',$play)->first();
-                return "<span class='blue-text'>$cate_txt->name - </span><span class='blue-text'>$play_txt->name</span> @ <span class='red-text'>$bet->play_odds</span>";
+//                if($bet->bet_game_id == 1){
+//                    $weishu = strlen((int)$bet->bet_game_id.$bet->bet_playcate_id);
+//                    $playid_weishu = strlen((int)$bet->bet_play_id);
+//                    $playCate = $bet->bet_playcate_id;
+//                    $play = substr($bet->bet_play_id,$weishu,$playid_weishu-$weishu);
+//                }
+//                if($bet->bet_game_id == 80 || $bet->bet_game_id == 81 || $bet->bet_game_id == 82 || $bet->bet_game_id == 99 || $bet->bet_game_id == 90 || $bet->bet_game_id == 91){
+//                    $playCate = substr($bet->bet_play_id,2,3);
+//                    $play = substr($bet->bet_play_id,5,4);
+//                }
+//                if($bet->bet_game_id == 50){
+//                    $playCate = substr($bet->bet_play_id,2,2);
+//                    $play = substr($bet->bet_play_id,4,3);
+//                }
+//                if($bet->bet_game_id == 66 || $bet->bet_game_id == 65){
+//                    $playCate = substr($bet->bet_play_id,2,2);
+//                    $play = substr($bet->bet_play_id,4,4);
+//                }
+//                $cate_txt = PlayCates::where('gameId',$bet->game_id)->where('id',$playCate)->first();
+//                $play_txt = Play::where('gameId',$bet->game_id)->where('id',$play)->first();
+//                return "<span class='blue-text'>$cate_txt->name - </span><span class='blue-text'>$play_txt->name</span> @ <span class='red-text'>$bet->play_odds</span>";
             })
             ->editColumn('rebate',function ($bet){
                 return '0';
             })
             ->editColumn('bet_money',function ($bet){
-                return '<span class="green-text">'.$bet->bet_money.'</span>';
+                return '<span class="green-text">'.$bet->bet_bet_money.'</span>';
             })
             ->editColumn('none1',function ($bet){
                 return '-';
@@ -472,13 +475,13 @@ class BetDataController extends Controller
                 return '-';
             })
             ->editColumn('bunko',function ($bet){
-                if($bet->bunko == 0){
+                if($bet->bet_bunko == 0){
                     return '<span class="gary-text">未结算</span>';
                 }
-                if($bet->bunko < 0){
-                    return '<span class="red-text">'.$bet->bunko.'</span>';
+                if($bet->bet_bunko < 0){
+                    return '<span class="red-text">'.$bet->bet_bunko.'</span>';
                 } else {
-                    return '<span class="blue-text">'.$bet->bunko.'</span>';
+                    return '<span class="blue-text">'.$bet->bet_bunko.'</span>';
                 }
             })
             ->editColumn('dongjie',function ($bet){
