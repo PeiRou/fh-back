b9534a970452d0260bd1b3e2dab37775ea1240c8
vincent-lee <laomumeme@gmail.com>
2018-07-06 16:24:37 +0800

    1

--- app/Helpers/Play.php
+++ app/Helpers/Play.php
@@ -9,10 +9,14 @@
 namespace App\Helpers;
 
 
+use Illuminate\Support\Facades\DB;
+
 class Play
 {
-    public function handle()
+    public function handle($gameId,$playCate,$play,$odds)
     {
-        return 1;
+        $cate_txt = DB::table('play_cate')->where('gameId',$gameId)->where('id',$playCate)->first();
+        $play_txt = DB::table('play')->where('gameId',$gameId)->where('id',$play)->first();
+        return "<span class='blue-text'>$cate_txt->name - </span><span class='blue-text'>$play_txt->name</span> @ <span class='red-text'>$odds</span>";
     }
 }
