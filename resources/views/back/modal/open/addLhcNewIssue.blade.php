0c77eadad482f13b00aa95b08374d8eefeeec474
vincent-lee <laomumeme@gmail.com>
2018-07-07 21:28:40 +0800

    1

--- resources/views/back/modal/open/addLhcNewIssue.blade.php
+++ resources/views/back/modal/open/addLhcNewIssue.blade.php
@@ -22,6 +22,37 @@
 <script src="/js/moment.min.js"></script>
 <script src="/js/daterangepicker.js"></script>
 <script>
+    $('input[name="end_time"]').daterangepicker({
+        locale: {
+            format: 'YYYY-MM-DD',
+            "applyLabel": "确认",
+            "cancelLabel": "取消",
+            "daysOfWeek": [
+                "日",
+                "一",
+                "二",
+                "三",
+                "四",
+                "五",
+                "六"
+            ],
+            "monthNames": [
+                "一月",
+                "二月",
+                "三月",
+                "四月",
+                "五月",
+                "六月",
+                "七月",
+                "八月",
+                "九月",
+                "十月",
+                "十一月",
+                "十二月"
+            ]
+        }
+    });
+
     $('#addLhcNewIssueForm').formValidation({
         framework: 'semantic',
         icon: {
