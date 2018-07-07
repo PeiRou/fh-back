86a381317295770b939e62a5726338d570320c84
vincent-lee <laomumeme@gmail.com>
2018-07-07 20:44:22 +0800

    1

--- app/Http/Controllers/Back/Data/openHistoryController.php
+++ app/Http/Controllers/Back/Data/openHistoryController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Back\Data;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\DataTables;
 
 class openHistoryController extends Controller
 {
     public function lhc(Request $request)
     {
-        
+        $lhc = DB::table('game_lhc')->get();
+        return DataTables::of($lhc)
+            ->make(true);
     }
 }
