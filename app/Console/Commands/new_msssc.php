4caac549509288071b1d753ba6925c47d0f06746
laomumeme <laomumeme@gmail.com>
2018-07-09 03:01:20 +0800

    Initial commit

--- app/Console/Commands/new_msssc.php
+++ app/Console/Commands/new_msssc.php
@@ -50,7 +50,7 @@ class new_msssc extends Command
     {
         $action = $this->argument('action');
         if($action !== ''){
-            Redis::select(8); //杀-专用redis库
+            Redis::select(0); //杀-专用redis库
             Redis::setex('sha:msssc',10,$action);
         }
         $this->go();
