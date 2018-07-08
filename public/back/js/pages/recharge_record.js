fdd8b136643bccf52c059427048700120210cc16
laomumeme <laomumeme@gmail.com>
2018-07-09 00:36:17 +0800

    Initial commit

--- public/back/js/pages/recharge_record.js
+++ public/back/js/pages/recharge_record.js
@@ -3,30 +3,30 @@ $(function () {
     $('#menu-financeManage').addClass('nav-show');
     $('#menu-financeManage-rechargeRecord').addClass('active');
 
-    context.init({preventDoubleContext: false});
-    context.settings({compress: true});
-    context.attach('html', [
-        {header: 'Compressed Menu'},
-        {text: 'Back', href: '#'},
-        {text: 'Reload', href: '#'},
-        {divider: true},
-        {text: 'Save As', href: '#'},
-        {text: 'Print', href: '#'},
-        {text: 'View Page Source', href: '#'},
-        {text: 'View Page Info', href: '#'},
-        {divider: true},
-        {text: 'Inspect Element', href: '#'},
-        {divider: true},
-        {text: 'Disable This Menu', action: function(e){
-                e.preventDefault();
-                context.destroy('html');
-                alert('html contextual menu destroyed!');
-            }},
-        {text: 'Donate?', action: function(e){
-                e.preventDefault();
-                $('#donate').submit();
-            }}
-    ]);
+    // context.init({preventDoubleContext: false});
+    // context.settings({compress: true});
+    // context.attach('html', [
+    //     {header: 'Compressed Menu'},
+    //     {text: 'Back', href: '#'},
+    //     {text: 'Reload', href: '#'},
+    //     {divider: true},
+    //     {text: 'Save As', href: '#'},
+    //     {text: 'Print', href: '#'},
+    //     {text: 'View Page Source', href: '#'},
+    //     {text: 'View Page Info', href: '#'},
+    //     {divider: true},
+    //     {text: 'Inspect Element', href: '#'},
+    //     {divider: true},
+    //     {text: 'Disable This Menu', action: function(e){
+    //             e.preventDefault();
+    //             context.destroy('html');
+    //             alert('html contextual menu destroyed!');
+    //         }},
+    //     {text: 'Donate?', action: function(e){
+    //             e.preventDefault();
+    //             $('#donate').submit();
+    //         }}
+    // ]);
 
     getTotalRecharge();
 
