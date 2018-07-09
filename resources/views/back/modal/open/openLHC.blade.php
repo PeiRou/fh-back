503af990079b207171e79c4d80716fdb77e291f6
vincent-lee <laomumeme@gmail.com>
2018-07-09 16:07:03 +0800

    1

--- resources/views/back/modal/open/openLHC.blade.php
+++ resources/views/back/modal/open/openLHC.blade.php
@@ -37,8 +37,8 @@
                 <option value=""></option>
             </select>
         </div>
-        <div class="ui input icon">
-            <span style="width: 70px;margin-left: 10px;">特码：</span>
+        <div class="ui input icon" style="width: 104px !important;">
+            <span style="width: 70px;margin-left: 10px;line-height: 37px;">特码：</span>
             <select class="ui dropdown" name="nums">
                 <option value=""></option>
             </select>
@@ -49,7 +49,7 @@
 
 <script>
     $(function () {
-        var selectNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49];
+        var selectNum = ['',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49];
         var str;
         for(var i = 0;i<selectNum.length;i++){
             str += '<option value="'+selectNum[i]+'">'+selectNum[i]+'</option>'
