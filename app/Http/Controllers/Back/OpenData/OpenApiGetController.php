308e246513b984fa89bf809b29bee5f4b8698ae1
vincent-lee <laomumeme@gmail.com>
2018-07-09 17:44:23 +0800

    1

--- app/Http/Controllers/Back/OpenData/OpenApiGetController.php
+++ app/Http/Controllers/Back/OpenData/OpenApiGetController.php
@@ -15,11 +15,15 @@ class OpenApiGetController extends Controller
             $res = $http->request('get','http://vip.jiangyuan365.com/K25ae456c03d2df/18077/xglhc.json');
             $json = json_decode((string) $res->getBody(), true);
             return response()->json([
-                'issue'=> $json
+                'code' => 200,
+                'data'=> $json,
+                'status' => true
             ]);
         } catch (\Exception $e){
             return response()->json([
-                'issue'=> $e->getCode()
+                'code'=> $e->getCode(),
+                'status' => false,
+                'msg' => '获取失败，原因1.本期暂未开奖，2.接口数据返回异常'
             ]);
         }
     }
