fa50efed059d03403e963744569e565815bb923b
vincent-lee <laomumeme@gmail.com>
2018-07-09 15:48:56 +0800

    1

--- resources/views/back/modal/open/openLHC.blade.php
+++ resources/views/back/modal/open/openLHC.blade.php
@@ -13,13 +13,19 @@
             </select>
         </div>
         <div class="ui input icon">
-            <input type="text" name="end_time" value=""/>
+            <select name="" id="nums">
+                <option value=""></option>
+            </select>
         </div>
         <div class="ui input icon">
-            <input type="text" name="end_time" value=""/>
+            <select name="" id="nums">
+                <option value=""></option>
+            </select>
         </div>
         <div class="ui input icon">
-            <input type="text" name="end_time" value=""/>
+            <select name="" id="nums">
+                <option value=""></option>
+            </select>
         </div>
     </div>
     <input type="hidden" name="id" value="{{ $lhc->id }}">
@@ -29,7 +35,7 @@
     $(function () {
         var selectNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49];
         var str;
-        for(var i = 1;i<=selectNum.length;i++){
+        for(var i = 0;i<=selectNum.length;i++){
             str += '<option value="'+selectNum[i]+'">'+selectNum[i]+'</option>'
         }
         $('#nums').html(str);
