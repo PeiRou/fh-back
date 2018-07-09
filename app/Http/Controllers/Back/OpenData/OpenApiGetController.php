9f24e7f376e97abcebd7bd148a48e67b53b69c37
vincent-lee <laomumeme@gmail.com>
2018-07-09 17:35:07 +0800

    1

--- app/Http/Controllers/Back/OpenData/OpenApiGetController.php
+++ app/Http/Controllers/Back/OpenData/OpenApiGetController.php
@@ -11,7 +11,7 @@ class OpenApiGetController extends Controller
     public function lhc($issue = '')
     {
         $http = new Client();
-        $res = $http->request('get','http://vip.jiangyuan365.com/K25ae456c03d2df/18074/xglhc.json');
+        $res = $http->request('get','http://vip.jiangyuan365.com/K25ae456c03d2df/18077/xglhc.json');
         $json = json_decode((string) $res->getBody(), true);
         return response()->json([
             'issue'=> $json
