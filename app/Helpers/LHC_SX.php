0a1367a495654ae82481f58c455fde56191d8326
vincent-lee <laomumeme@gmail.com>
2018-07-09 19:09:57 +0800

    1

--- app/Helpers/LHC_SX.php
+++ app/Helpers/LHC_SX.php
@@ -50,4 +50,19 @@ class LHC_SX
             return '龙';
         }
     }
+
+    public function sebo($num){
+        $red = [1,2,7,8,12,13,18,19,23,24,29,30,34,35,40,45,46];
+        $blue = [3,4,9,10,14,15,20,25,26,31,36,37,41,42,47,48];
+        $green = [5,6,11,16,17,21,22,27,28,32,33,38,39,43,44,49];
+        if(in_array($num,$red)){
+            return 'r';
+        }
+        if(in_array($num,$blue)){
+            return 'b';
+        }
+        if(in_array($num,$green)){
+            return 'g';
+        }
+    }
 }
