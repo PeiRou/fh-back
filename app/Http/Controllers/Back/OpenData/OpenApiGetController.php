e256190826e493afbb55b05d29216b0ae50a4347
vincent-lee <laomumeme@gmail.com>
2018-07-09 17:12:11 +0800

    1

--- app/Http/Controllers/Back/OpenData/OpenApiGetController.php
+++ app/Http/Controllers/Back/OpenData/OpenApiGetController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers\Back\OpenData;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
 
 class OpenApiGetController extends Controller
 {
     public function lhc($issue = '')
     {
+        $http = new Client();
+        $res = $http->request('get','http://vip.jiangyuan365.com/K25ae456c03d2df/18074/xglhc.json');
+        $json = json_decode((string) $res->getBody(), true);
         return response()->json([
-            'issue'=> $issue
+            'issue'=> $json
         ]);
     }
 }
