69c4cb909c99e5a1c1cd0d2e84e733387366ec9f
vincent-lee <laomumeme@gmail.com>
2018-07-09 17:40:55 +0800

    1

--- app/Http/Controllers/Back/OpenData/OpenApiGetController.php
+++ app/Http/Controllers/Back/OpenData/OpenApiGetController.php
@@ -11,10 +11,16 @@ class OpenApiGetController extends Controller
     public function lhc($issue = '')
     {
         $http = new Client();
-        $res = $http->request('get','http://vip.jiangyuan365.com/K25ae456c03d2df/18077/xglhc.json');
-        $json = json_decode((string) $res->getBody(), true);
-        return response()->json([
-            'issue'=> $json
-        ]);
+        try{
+            $res = $http->request('get','http://vip.jiangyuan365.com/K25ae456c03d2df/18077/xglhc.json');
+            $json = json_decode((string) $res->getBody(), true);
+            return response()->json([
+                'issue'=> $json
+            ]);
+        } catch (\Exception $e){
+            return response()->json([
+                'issue'=> $e->getMessage()
+            ]);
+        }
     }
 }
