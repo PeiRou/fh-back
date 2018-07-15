6eab50338fe4c643cdd6d1515e195d4a7524011d
vincent-lee <laomumeme@gmail.com>
2018-07-15 19:00:06 +0800

    zoe

--- public/back/js/pages/recharge_record.js
+++ public/back/js/pages/recharge_record.js
@@ -6,26 +6,19 @@ $(function () {
     context.init({preventDoubleContext: false});
     context.settings({compress: true});
     context.attach('#rechargeRecordTable', [
-        {header: 'Compressed Menu'},
-        {text: 'Back', href: '#'},
-        {text: 'Reload', href: '#'},
+        {header: '便捷菜单'},
+        {text: '会员充值', href: '#'},
+        {text: '会员信息', href: '#'},
         {divider: true},
-        {text: 'Save As', href: '#'},
-        {text: 'Print', href: '#'},
-        {text: 'View Page Source', href: '#'},
-        {text: 'View Page Info', href: '#'},
-        {divider: true},
-        {text: 'Inspect Element', href: '#'},
-        {divider: true},
-        {text: 'Disable This Menu', action: function(e){
-                e.preventDefault();
-                context.destroy('html');
-                alert('html contextual menu destroyed!');
-            }},
-        {text: 'Donate?', action: function(e){
-                e.preventDefault();
-                $('#donate').submit();
-            }}
+        // {text: 'Disable This Menu', action: function(e){
+        //         e.preventDefault();
+        //         context.destroy('html');
+        //         alert('html contextual menu destroyed!');
+        //     }},
+        // {text: 'Donate?', action: function(e){
+        //         e.preventDefault();
+        //         $('#donate').submit();
+        //     }}
     ]);
 
     getTotalRecharge();
