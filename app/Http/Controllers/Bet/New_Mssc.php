fbe58887ff35d590b7341a4996d8b9fd48f8fbb5
laomumeme <laomumeme@gmail.com>
2018-07-18 11:51:26 +0800

    Initial commit

--- app/Http/Controllers/Bet/New_Mssc.php
+++ app/Http/Controllers/Bet/New_Mssc.php
@@ -1175,18 +1175,21 @@ class New_Mssc
             $ids = implode(',',$users);
             $bets = implode(',',$betsId);
             //\Log::info($ids);
-            $sql .= "END WHERE id IN (0,$ids)";
-            //\Log::info($sql);
-            $up = DB::statement($sql);
-            if($up == 1){
-                $sql_bet_status = "UPDATE bet SET status = 2 WHERE `bet_id` IN ($bets)";
-                $update_bet_status = DB::statement($sql_bet_status);
-                if($update_bet_status == 1){
-                    return 1;
+            if($ids && isset($ids)){
+                $sql .= "END WHERE id IN (0,$ids)";
+                //\Log::info($sql);
+                $up = DB::statement($sql);
+                if($up == 1){
+                    $sql_bet_status = "UPDATE bet SET status = 2 WHERE `bet_id` IN ($bets)";
+                    $update_bet_status = DB::statement($sql_bet_status);
+                    if($update_bet_status == 1){
+                        return 1;
+                    }
+                } else {
+                    \Log::info('更新用户余额，失败！');
                 }
-            } else {
-                \Log::info('更新用户余额，失败！');
             }
+
         } else {
             \Log::info('秒速赛车已结算过，已阻止！');
         }
