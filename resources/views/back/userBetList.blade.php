d449bb822575ae6e0ce91ac7f63ff3ee5ef85f20
vincent-lee <laomumeme@gmail.com>
2018-07-20 23:22:06 +0800

    zoe upd

--- resources/views/back/userBetList.blade.php
+++ resources/views/back/userBetList.blade.php
@@ -400,9 +400,9 @@
         $('#openH_'+id).show();
     }
 
-    function hideOpenHistory(gameId,issue,id){
-        $('#openH_'+id).hide();
-    }
+    // function hideOpenHistory(gameId,issue,id){
+    //     $('#openH_'+id).hide();
+    // }
 
     $('#date').on('change',function () {
         var data = $(this).val();
