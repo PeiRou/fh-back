4063a9fe764901a7e9e3f39c772f7df5ebb409bd
vincent-lee <laomumeme@gmail.com>
2018-07-26 20:30:20 +0800

    zoe upd

--- resources/views/back/handicapSetting.blade.php
+++ resources/views/back/handicapSetting.blade.php
@@ -32,7 +32,7 @@
                         <li data-tag="first" data-id="4">新疆时时彩</li>
                         <li data-tag="first" data-id="5">天津时时彩</li>
                         <li data-tag="first" data-id="60">广东快乐十分</li>
-                        <li data-tag="first" data-id="10">江苏骰宝(快3)</li>
+                        <li data-tag="first" data-id="10">江苏快3</li>
                         <li data-tag="first" data-id="61">重庆幸运农场</li>
                         <li data-tag="first" data-id="65">北京快乐8</li>
                         <li data-tag="first" data-id="21">广东十一选五</li>
