2c30bc8dcfe84bb5bb3424ddecb620da39f12f0f
vincent-lee <laomumeme@gmail.com>
2018-07-27 23:20:48 +0800

    zoe upd

--- app/Http/Controllers/Inner/TestController.php
+++ app/Http/Controllers/Inner/TestController.php
@@ -13,14 +13,30 @@ class TestController extends Controller
         $arrOpenCode = explode(',',$openCode);
         $playCate = 217;
         $isBaoZi = 0;
+        $ETH_arr = [
+            11 => 4311,
+            22 => 4312,
+            33 => 4313,
+            44 => 4314,
+            55 => 4315,
+            66 => 4316,
+        ];
         if((int)$arrOpenCode[0] == (int)$arrOpenCode[1] && (int)$arrOpenCode[0] == (int)$arrOpenCode[2]){
             $isBaoZi = 1;
         }
         if((int)$arrOpenCode[0] == (int)$arrOpenCode[1] && $isBaoZi == 0){
-            echo '前二'.$arrOpenCode[0];
+            foreach ($ETH_arr as $k => $v){
+                if((int)$arrOpenCode[0] == $k){
+                    echo $v;
+                }
+            }
         }
         if((int)$arrOpenCode[1] == (int)$arrOpenCode[2] && $isBaoZi == 0){
-            echo '后二'.$arrOpenCode[0];
+            foreach ($ETH_arr as $k => $v){
+                if((int)$arrOpenCode[0] == $k){
+                    echo $v;
+                }
+            }
         }
     }
 }
