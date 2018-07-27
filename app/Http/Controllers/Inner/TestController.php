d25ea1224ead530b42e8f2b03a27501a0dd8d1cc
vincent-lee <laomumeme@gmail.com>
2018-07-28 00:33:52 +0800

    zoe upd

--- app/Http/Controllers/Inner/TestController.php
+++ app/Http/Controllers/Inner/TestController.php
@@ -14,22 +14,16 @@ class TestController extends Controller
         $playCate = 219;
         $PD_NUM = (int)$arrOpenCode[0] + (int)$arrOpenCode[1] + (int)$arrOpenCode[2];
         $PD_GEWEI = $PD_NUM % 10;
-        $PD_arr = [
-            0 => 4323,
-            6 => 4323,
-            7 => 4323,
-            8 => 4323,
-            9 => 4323,
-            1 => 4324,
-            2 => 4324,
-            3 => 4324,
-            4 => 4324,
-            5 => 4324,
-        ];
-        foreach ($PD_arr as $k => $v){
+        $PD_DX_arr = [0 => 4323, 6 => 4323, 7 => 4323, 8 => 4323, 9 => 4323, 1 => 4324, 2 => 4324, 3 => 4324, 4 => 4324, 5 => 4324];
+        foreach ($PD_DX_arr as $k => $v){
             if($PD_GEWEI == $k){
-                echo $v;
+                echo '大小'.$v;
             }
+        }
+        if($PD_GEWEI%2 == 0){
+            echo "双";
+        } else {
+            echo '单';
         }
     }
 }
