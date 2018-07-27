311e32a23ac808d13cb16791cfae7a286e1a360a
vincent-lee <laomumeme@gmail.com>
2018-07-27 22:15:27 +0800

    zoe upd nsg center

--- app/Http/Controllers/Inner/TestController.php
+++ app/Http/Controllers/Inner/TestController.php
@@ -9,10 +9,13 @@ class TestController extends Controller
 {
     public function lhc()
     {
+        $HZ = 10;
         $HZDS_arr = [[3,4279],[4,4280],[5,4281],[6,4282],[7,4283],[8,4284],[9,4285],[10,4286],[11,4287],[12,4288],[13,4289],[14,4290],[15,4291],[16,4292],[17,4293],[18,4294]];
         foreach ($HZDS_arr as $k => $v){
             foreach ($v as $kk => $vv){
-                echo $vv;
+                if($HZ == $vv[0]){
+                    echo $vv[1];
+                }
             }
         }
     }
