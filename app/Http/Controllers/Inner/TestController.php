f287e961bfe91585ed66758a57d850ce89747e6f
vincent-lee <laomumeme@gmail.com>
2018-07-28 00:31:35 +0800

    zoe upd

--- app/Http/Controllers/Inner/TestController.php
+++ app/Http/Controllers/Inner/TestController.php
@@ -13,6 +13,23 @@ class TestController extends Controller
         $arrOpenCode = explode(',',$openCode);
         $playCate = 219;
         $PD_NUM = (int)$arrOpenCode[0] + (int)$arrOpenCode[1] + (int)$arrOpenCode[2];
-        echo $PD_NUM % 10;
+        $PD_GEWEI = $PD_NUM % 10;
+        $PD_arr = [
+            0 => 4323,
+            6 => 4323,
+            7 => 4323,
+            8 => 4323,
+            9 => 4323,
+            1 => 4324,
+            2 => 4324,
+            3 => 4324,
+            4 => 4324,
+            5 => 4324,
+        ];
+        foreach ($PD_arr as $k => $v){
+            if($PD_GEWEI == $k){
+                echo $v;
+            }
+        }
     }
 }
