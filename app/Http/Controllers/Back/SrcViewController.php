b0794fd78b87a346651b393dec99f638cceffa4f
laomumeme <laomumeme@gmail.com>
2018-08-05 02:28:07 +0800

    zoe upd

--- app/Http/Controllers/Back/SrcViewController.php
+++ app/Http/Controllers/Back/SrcViewController.php
@@ -86,7 +86,7 @@ class SrcViewController extends Controller
         $lastMonthRegUsers = DB::table('users')->where('testFlag',0)->whereRaw('PERIOD_DIFF( date_format( now( ) , "%Y%m" ) , date_format( created_at, "%Y%m" ) ) =1')->count();
         $todayRechargesUser = DB::table('users')->where('testFlag',0)->where('PayTimes',1)->whereDate('created_at',date('Y-m-d'))->count();
         $yesterdayRechargesUser = DB::table('users')->where('testFlag',0)->where('PayTimes',1)->whereDate('created_at',Carbon::now()->addDays(-1)->toDateString())->count();
-        $monthRechargesUser = DB::table('users')->where('testFlag',0)->where('PayTimes',1)->whereDate('created_at',date('Y-m'))->count();
+        $monthRechargesUser = DB::table('users')->where('testFlag',0)->where('PayTimes',1)->whereRaw('DATE_FORMAT(created_at, "%Y%m" ) = DATE_FORMAT( CURDATE( ) , "%Y%m" )')->count();
 
         return view('back.user',compact('aid','gaid','levels','agent','allUser','todayRegUsers','monthRegUsers','lastMonthRegUsers','todayRechargesUser','yesterdayRegUsers','yesterdayRechargesUser','monthRechargesUser'));
     }
