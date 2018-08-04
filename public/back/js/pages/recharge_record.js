537545994975cb1cdc64e75fd153a91f8a554648
vincent-lee <laomumeme@gmail.com>
2018-08-04 23:52:04 +0800

    zoe upd

--- public/back/js/pages/recharge_record.js
+++ public/back/js/pages/recharge_record.js
@@ -2,51 +2,13 @@ $(function () {
     $('#menu-financeManage').addClass('nav-show');
     $('#menu-financeManage-rechargeRecord').addClass('active');
 
-    var btns = document.querySelectorAll('.btn');
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener('mouseleave', clearTooltip);
-        btns[i].addEventListener('blur', clearTooltip);
-    }
-
     var clipboard = new ClipboardJS('.copyUsername');
     clipboard.on('success', function(e) {
-
+        alert('已复制')
     });
     clipboard.on('error', function(e) {
-
+        alert('复制失败')
     });
-    var clipboardSnippets = new ClipboardJS('[data-clipboard-snippet]', {
-        target: function(trigger) {
-            return trigger.nextElementSibling;
-        }
-    });
-    clipboardSnippets.on('success', function(e) {
-        e.clearSelection();
-        showTooltip(e.trigger, '已复制');
-    });
-    clipboardSnippets.on('error', function(e) {
-        showTooltip(e.trigger, fallbackMessage(e.action));
-    });
-    function clearTooltip(e) {
-        e.currentTarget.setAttribute('class', 'btn');
-        e.currentTarget.removeAttribute('aria-label');
-    }
-    function showTooltip(elem, msg) {
-        elem.setAttribute('class', 'btn tooltipped tooltipped-s');
-        elem.setAttribute('aria-label', msg);
-    }
-    function fallbackMessage(action) {
-        var actionMsg = '';
-        var actionKey = (action === 'cut' ? 'X' : 'C');
-        if (/iPhone|iPad/i.test(navigator.userAgent)) {
-            actionMsg = 'No support :(';
-        } else if (/Mac/i.test(navigator.userAgent)) {
-            actionMsg = 'Press ⌘-' + actionKey + ' to ' + action;
-        } else {
-            actionMsg = 'Press Ctrl-' + actionKey + ' to ' + action;
-        }
-        return actionMsg;
-    }
     // context.init({preventDoubleContext: false});
     // context.settings({compress: true});
     // context.attach('#rechargeRecordTable', [
