76fb633037f48d5fa73a0292f4c365706b77802f
vincent-lee <laomumeme@gmail.com>
2018-08-04 23:30:27 +0800

    zoe upd

--- public/back/js/pages/recharge_record.js
+++ public/back/js/pages/recharge_record.js
@@ -2,41 +2,41 @@ $(function () {
     $('#menu-financeManage').addClass('nav-show');
     $('#menu-financeManage-rechargeRecord').addClass('active');
 
-    // var clipboard = new ClipboardJS('.copyUsername');
-    // clipboard.on('success', function(e) {
-    //
+    var clipboard = new ClipboardJS('.copyUsername');
+    clipboard.on('success', function(e) {
+
+    });
+    clipboard.on('error', function(e) {
+
+    });
+    // var clipboardSnippets = new ClipboardJS('[data-clipboard-snippet]', {
+    //     target: function(trigger) {
+    //         return trigger.nextElementSibling;
+    //     }
     // });
-    // clipboard.on('error', function(e) {
-    //
+    // clipboardSnippets.on('success', function(e) {
+    //     e.clearSelection();
+    //     showTooltip(e.trigger, 'Copied!');
     // });
-    var clipboardSnippets = new ClipboardJS('[data-clipboard-snippet]', {
-        target: function(trigger) {
-            return trigger.nextElementSibling;
-        }
-    });
-    clipboardSnippets.on('success', function(e) {
-        e.clearSelection();
-        showTooltip(e.trigger, 'Copied!');
-    });
-    clipboardSnippets.on('error', function(e) {
-        showTooltip(e.trigger, fallbackMessage(e.action));
-    });
-    function showTooltip(elem, msg) {
-        elem.setAttribute('class', 'btn tooltipped tooltipped-s');
-        elem.setAttribute('aria-label', msg);
-    }
-    function fallbackMessage(action) {
-        var actionMsg = '';
-        var actionKey = (action === 'cut' ? 'X' : 'C');
-        if (/iPhone|iPad/i.test(navigator.userAgent)) {
-            actionMsg = 'No support :(';
-        } else if (/Mac/i.test(navigator.userAgent)) {
-            actionMsg = 'Press ⌘-' + actionKey + ' to ' + action;
-        } else {
-            actionMsg = 'Press Ctrl-' + actionKey + ' to ' + action;
-        }
-        return actionMsg;
-    }
+    // clipboardSnippets.on('error', function(e) {
+    //     showTooltip(e.trigger, fallbackMessage(e.action));
+    // });
+    // function showTooltip(elem, msg) {
+    //     elem.setAttribute('class', 'btn tooltipped tooltipped-s');
+    //     elem.setAttribute('aria-label', msg);
+    // }
+    // function fallbackMessage(action) {
+    //     var actionMsg = '';
+    //     var actionKey = (action === 'cut' ? 'X' : 'C');
+    //     if (/iPhone|iPad/i.test(navigator.userAgent)) {
+    //         actionMsg = 'No support :(';
+    //     } else if (/Mac/i.test(navigator.userAgent)) {
+    //         actionMsg = 'Press ⌘-' + actionKey + ' to ' + action;
+    //     } else {
+    //         actionMsg = 'Press Ctrl-' + actionKey + ' to ' + action;
+    //     }
+    //     return actionMsg;
+    // }
     // context.init({preventDoubleContext: false});
     // context.settings({compress: true});
     // context.attach('#rechargeRecordTable', [
