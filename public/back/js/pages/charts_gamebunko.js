7794733075b7e12b295d3bc34f497fbbcd67c293
vincent-lee <laomumeme@gmail.com>
2018-08-05 19:49:46 +0800

    zoe upd

--- public/back/js/pages/charts_gamebunko.js
+++ public/back/js/pages/charts_gamebunko.js
@@ -99,8 +99,8 @@ $(function () {
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
 
-    function refreshCharts() {
-        myChart.showLoading();
-        loadData;
-    }
 });
+
+function refreshCharts() {
+    loadData;
+}
