87ad690ca1fa5fb2927a0fccad17bec3acc64bd2
vincent-lee <laomumeme@gmail.com>
2018-08-05 21:23:22 +0800

    zoe upd

--- public/back/js/pages/charts_recharges.js
+++ public/back/js/pages/charts_recharges.js
@@ -32,22 +32,25 @@ $(function () {
         xAxis: [
             {
                 type: 'category',
-                data: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+                data: ['00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00'],
                 axisPointer: {
                     type: 'shadow'
                 }
             }
         ],
         yAxis: [
+            // {
+            //     type: 'value',
+            //     name: '金额',
+            //     min: 0,
+            //     max: 250,
+            //     interval: 50,
+            //     axisLabel: {
+            //         formatter: '{value} ml'
+            //     }
+            // }
             {
-                type: 'value',
-                name: '金额',
-                min: 0,
-                max: 250,
-                interval: 50,
-                axisLabel: {
-                    formatter: '{value} ml'
-                }
+                type : 'value'
             }
         ],
         series: [
