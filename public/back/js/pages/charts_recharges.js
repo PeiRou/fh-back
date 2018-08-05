2bff6ce22e91c4e5bfad6e5ca6a6fab38b52cf07
vincent-lee <laomumeme@gmail.com>
2018-08-05 21:18:23 +0800

    zoe upd

--- public/back/js/pages/charts_recharges.js
+++ public/back/js/pages/charts_recharges.js
@@ -27,7 +27,7 @@ $(function () {
             }
         },
         legend: {
-            data:['蒸发量','降水量','平均温度']
+            data:['充值','提款']
         },
         xAxis: [
             {
@@ -41,41 +41,25 @@ $(function () {
         yAxis: [
             {
                 type: 'value',
-                name: '水量',
+                name: '金额',
                 min: 0,
                 max: 250,
                 interval: 50,
                 axisLabel: {
                     formatter: '{value} ml'
                 }
-            },
-            {
-                type: 'value',
-                name: '温度',
-                min: 0,
-                max: 25,
-                interval: 5,
-                axisLabel: {
-                    formatter: '{value} °C'
-                }
             }
         ],
         series: [
             {
-                name:'蒸发量',
+                name:'充值',
                 type:'bar',
                 data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
             },
             {
-                name:'降水量',
+                name:'提款',
                 type:'bar',
                 data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-            },
-            {
-                name:'平均温度',
-                type:'line',
-                yAxisIndex: 1,
-                data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
             }
         ]
     };
