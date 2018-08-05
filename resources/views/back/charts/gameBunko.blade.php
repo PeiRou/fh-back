0da5c46e13409539bfd7522ab420ea0ad2d22621
vincent-lee <laomumeme@gmail.com>
2018-08-05 18:58:48 +0800

    zoe upd

--- resources/views/back/charts/gameBunko.blade.php
+++ resources/views/back/charts/gameBunko.blade.php
@@ -3,9 +3,17 @@
 @section('title','盈亏统计')
 
 @section('content')
-
+    <div class="content-top">
+        <div class="breadcrumb">
+            <b>位置：</b>盈亏统计
+        </div>
+        <div class="content-top-buttons">
+            <span class="refresh-nav-btn" onclick="refreshTable('reportAgentTable')"><i class="iconfont">&#xe61d;</i></span>
+        </div>
+    </div>
 @endsection
 
 @section('page-js')
+    <script src="/vendor/echarts/echarts.js"></script>
     <script src="/back/js/pages/charts_gamebunko.js"></script>
 @endsection
