dffd91bf0718555332b0c541db620b096ce0484f
vincent-lee <laomumeme@gmail.com>
2018-08-05 16:04:11 +0800

    zoe upd

--- resources/views/back/reportBet.blade.php
+++ resources/views/back/reportBet.blade.php
@@ -3,5 +3,12 @@
 @section('title','投注报表')
 
 @section('content')
-
+    <div class="content-top">
+        <div class="breadcrumb">
+            <b>位置：</b>投注报表
+        </div>
+        <div class="content-top-buttons">
+            <span class="refresh-nav-btn" onclick="refreshTable('')"><i class="iconfont">&#xe61d;</i></span>
+        </div>
+    </div>
 @endsection
