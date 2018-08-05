7089c77eac26b2972f65f23d5a541e4ca9b2ad82
vincent-lee <laomumeme@gmail.com>
2018-08-05 19:12:27 +0800

    zoe upd

--- public/back/js/pages/charts_gamebunko.js
+++ public/back/js/pages/charts_gamebunko.js
@@ -16,7 +16,7 @@ $(function () {
             }
         },
         legend: {
-            data:['利润', '支出', '收入']
+            data:['盈亏']
         },
         grid: {
             left: '3%',
@@ -38,7 +38,7 @@ $(function () {
         ],
         series : [
             {
-                name:'利润',
+                name:'盈亏',
                 type:'bar',
                 label: {
                     normal: {
@@ -46,30 +46,7 @@ $(function () {
                         position: 'inside'
                     }
                 },
-                data:[200, 170, 240, 244, 200, 220, 210]
-            },
-            {
-                name:'收入',
-                type:'bar',
-                stack: '总量',
-                label: {
-                    normal: {
-                        show: true
-                    }
-                },
-                data:[320, 302, 341, 374, 390, 450, 420]
-            },
-            {
-                name:'支出',
-                type:'bar',
-                stack: '总量',
-                label: {
-                    normal: {
-                        show: true,
-                        position: 'left'
-                    }
-                },
-                data:[-120, -132, -101, -134, -190, -230, -210]
+                data:[200, 170, -240, 244, 200, -220, 210]
             }
         ]
     };
