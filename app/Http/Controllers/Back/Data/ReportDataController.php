a6dad83da30f4d9bd5f5e76e6d5dca261f06698a
vincent-lee <laomumeme@gmail.com>
2018-08-05 17:19:30 +0800

    zoe upd

--- app/Http/Controllers/Back/Data/ReportDataController.php
+++ app/Http/Controllers/Back/Data/ReportDataController.php
@@ -153,7 +153,7 @@ sum(case WHEN b.game_id in (90,91) then nn_view_money else(case when bunko >0 th
     {
         $starttime = '2018-08-05';
         $endtime = '2018-08-05';
-        $sql = "SELECT g.game_name,g.status,g.game_id, sum(b.bet_money) as sumMoney, COUNT(b.bet_id) AS countBets,count(DISTINCT(b.user_id)) as countMember, sum(case WHEN b.game_id in (90,91) then (case WHEN nn_view_money > 0 then bunko else 0 end) else(case WHEN bunko >0 then bunko else 0 end) end) as sumWinBunko, count(case WHEN b.game_id in (90,91) then (case WHEN nn_view_money > 0 then b.bet_id else Null end) else(case WHEN bunko >0 then b.bet_id else Null end) end) as countWinBunkoBet, count(DISTINCT(case WHEN b.game_id in (90,91) then (case WHEN nn_view_money > 0 then b.user_id else Null end) else(case WHEN bunko >0 then b.user_id else Null end) end)) as countWinBunkoMember, sum(case WHEN b.game_id in (90,91) then nn_view_money else(case when bunko >0 then bunko-bet_money else bunko end)end) as sumBunko FROM `game` AS g LEFT JOIN bet as b ON g.game_id = b.game_id  ";
+        $sql = "SELECT g.game_name,g.status,g.game_id, sum(b.bet_money) as sumMoney, COUNT(b.bet_id) AS countBets,count(DISTINCT(b.user_id)) as countMember, sum(case WHEN b.game_id in (90,91) then (case WHEN nn_view_money > 0 then bunko else 0 end) else(case WHEN bunko >0 then bunko else 0 end) end) as sumWinBunko, count(case WHEN b.game_id in (90,91) then (case WHEN nn_view_money > 0 then b.bet_id else Null end) else(case WHEN bunko >0 then b.bet_id else Null end) end) as countWinBunkoBet, count(DISTINCT(case WHEN b.game_id in (90,91) then (case WHEN nn_view_money > 0 then b.user_id else Null end) else(case WHEN bunko >0 then b.user_id else Null end) end)) as countWinBunkoMember, sum(case WHEN b.game_id in (90,91) then nn_view_money else(case when bunko >0 then bunko-bet_money else bunko end)end) as sumBunko FROM `game` AS g LEFT JOIN bet as b ON g.game_id = b.game_id and b.testFlag = 0 ";
         $whereBet = "";
         if(isset($starttime) && $starttime){
             $whereBet .= " and b.created_at >= '2018-08-05 00:00:00'";
@@ -162,7 +162,7 @@ sum(case WHEN b.game_id in (90,91) then nn_view_money else(case when bunko >0 th
             $whereBet .= " and b.created_at <= '2018-08-05 23:59:59'";
         }
         $sql .= $whereBet ;
-        $sql .= " WHERE 1 and b.testFlag = 0 GROUP BY g.game_id ";
+        $sql .= " WHERE 1 GROUP BY g.game_id ";
         $bet = DB::select($sql);
         return DataTables::of($bet)
             ->make(true);
