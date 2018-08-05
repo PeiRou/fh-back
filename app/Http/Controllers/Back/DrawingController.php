78115dcfaa6c432c8f6aacc3b8636ea0faa7ff2d
vincent-lee <laomumeme@gmail.com>
2018-08-05 14:06:27 +0800

    zoe upd

--- app/Http/Controllers/Back/DrawingController.php
+++ app/Http/Controllers/Back/DrawingController.php
@@ -7,6 +7,7 @@ use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Session;
 
 class DrawingController extends Controller
