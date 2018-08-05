b89703b456d84715c6f5f6e55c88c5c466c732a6
vincent-lee <laomumeme@gmail.com>
2018-08-05 19:10:10 +0800

    zoe upd

--- public/back/js/pages/charts_gamebunko.js
+++ public/back/js/pages/charts_gamebunko.js
@@ -9,19 +9,69 @@ $(function () {
         title: {
             text: 'ECharts 入门示例'
         },
-        tooltip: {},
+        tooltip : {
+            trigger: 'axis',
+            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
         legend: {
-            data:['销量']
+            data:['利润', '支出', '收入']
         },
-        xAxis: {
-            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
         },
-        yAxis: {},
-        series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-        }]
+        xAxis : [
+            {
+                type : 'value'
+            }
+        ],
+        yAxis : [
+            {
+                type : 'category',
+                axisTick : {show: false},
+                data : ['周一','周二','周三','周四','周五','周六','周日']
+            }
+        ],
+        series : [
+            {
+                name:'利润',
+                type:'bar',
+                label: {
+                    normal: {
+                        show: true,
+                        position: 'inside'
+                    }
+                },
+                data:[200, 170, 240, 244, 200, 220, 210]
+            },
+            {
+                name:'收入',
+                type:'bar',
+                stack: '总量',
+                label: {
+                    normal: {
+                        show: true
+                    }
+                },
+                data:[320, 302, 341, 374, 390, 450, 420]
+            },
+            {
+                name:'支出',
+                type:'bar',
+                stack: '总量',
+                label: {
+                    normal: {
+                        show: true,
+                        position: 'left'
+                    }
+                },
+                data:[-120, -132, -101, -134, -190, -230, -210]
+            }
+        ]
     };
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
