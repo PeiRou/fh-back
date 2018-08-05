2bf812e53ee3e90db2eb4e5997f05ac5dce7e9cd
vincent-lee <laomumeme@gmail.com>
2018-08-05 22:01:22 +0800

    zoe upd

--- public/back/js/pages/charts_recharges.js
+++ public/back/js/pages/charts_recharges.js
@@ -9,6 +9,15 @@ $(function () {
         title: {
             text: '24小时充值统计图表'
         },
+        tooltip: {
+            trigger: 'axis',
+            // axisPointer: {
+            //     type: 'cross',
+            //     crossStyle: {
+            //         color: '#999'
+            //     }
+            // }
+        },
         toolbox: {
             feature: {
                 dataView: {show: true, readOnly: false},
