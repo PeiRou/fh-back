2308109abe3774b2f0fa6415fadc7447dfcd186a
vincent-lee <laomumeme@gmail.com>
2018-08-06 17:42:23 +0800

    zoe kill

--- app/Http/Controllers/Inner/TestController.php
+++ app/Http/Controllers/Inner/TestController.php
@@ -13,6 +13,7 @@ class TestController extends Controller
         shuffle($nums);
         shuffle($nums);
         shuffle($nums);
-        return $nums;
+        $opennums = $nums[0].','.$nums[1].','.$nums[2].','.$nums[3].','.$nums[4].','.$nums[5].','.$nums[6].','.$nums[7].','.$nums[8].','.$nums[9];
+        return $opennums;
     }
 }
