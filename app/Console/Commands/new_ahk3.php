e95048b61579d49b41bb67f0209935d89cbc8ea0
vincent-lee <laomumeme@gmail.com>
2018-08-07 18:58:55 +0800

    zoe kill

--- app/Console/Commands/new_ahk3.php
+++ app/Console/Commands/new_ahk3.php
@@ -9,21 +9,21 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Storage;
 
-class new_bjk3 extends Command
+class new_ahk3 extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'new_bjk3';
+    protected $signature = 'new_ahk3';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = '北京快3';
+    protected $description = '安徽快3';
 
     /**
      * Create a new command instance.
@@ -42,7 +42,7 @@ class new_bjk3 extends Command
      */
     public function handle()
     {
-        $getFile    = Storage::disk('gameTime')->get('bjk3.json');
+        $getFile    = Storage::disk('gameTime')->get('ahk3.json');
         $data       = json_decode($getFile,true);
         $nowTime    = date('H:i:s');
         $filtered = collect($data)->first(function ($value, $key) use ($nowTime) {
@@ -53,28 +53,28 @@ class new_bjk3 extends Command
         });
         if($filtered!=null){
             $nowIssueTime = strtotime(date('Y-m-d').' '.$filtered['time']);
-            $getIssue = DB::table('game_bjk3')->whereRaw('unix_timestamp(opentime) = '.$nowIssueTime)->first();
+            $getIssue = DB::table('game_ahk3')->whereRaw('unix_timestamp(opentime) = '.$nowIssueTime)->first();
             $nextIssue = $getIssue->issue;
 
-            if(strtotime($filtered['time']) == strtotime('23:50:00')){
+            if(strtotime($filtered['time']) == strtotime('22:00:00')){
                 $nextDay = Carbon::parse(date('Y-m-d'))->addDay(1)->toDateTimeString();
-                $nextIssueEndTime = date('Y-m-d',strtotime($nextDay)).' 09:08:00';
-                $nextIssueLotteryTime = date('Y-m-d',strtotime($nextDay)).' 09:10:00';
+                $nextIssueEndTime = date('Y-m-d',strtotime($nextDay)).' 08:48:00';
+                $nextIssueLotteryTime = date('Y-m-d',strtotime($nextDay)).' 08:50:00';
             } else {
                 $nextIssueEndTime = Carbon::parse($getIssue->opentime)->addMinute(8)->toDateTimeString();
                 $nextIssueLotteryTime = Carbon::parse($getIssue->opentime)->addMinutes(10)->toDateTimeString();
             }
 
-            Redis::set('bjk3:nextIssue',(int)$nextIssue+1);
-            Redis::set('bjk3:nextIssueLotteryTime',strtotime($nextIssueLotteryTime));
-            Redis::set('bjk3:nextIssueEndTime',strtotime($nextIssueEndTime));
+            Redis::set('ahk3:nextIssue',(int)$nextIssue+1);
+            Redis::set('ahk3:nextIssueLotteryTime',strtotime($nextIssueLotteryTime));
+            Redis::set('ahk3:nextIssueEndTime',strtotime($nextIssueEndTime));
         }
-        $url = Config::get('website.guanServerUrl').'bjk3';
+        $url = Config::get('website.guanServerUrl').'ahk3';
         $html = json_decode(file_get_contents($url),true);
-        $redis_issue = Redis::get('bjk3:issue');
+        $redis_issue = Redis::get('ahk3:issue');
         if($redis_issue !== $html[0]['issue']){
             try{
-                $up = DB::table('game_bjk3')->where('issue',$html[0]['issue'])
+                $up = DB::table('game_ahk3')->where('issue',$html[0]['issue'])
                     ->update([
                         'is_open' => 1,
                         'year'=> date('Y'),
@@ -83,7 +83,7 @@ class new_bjk3 extends Command
                         'opennum' => $html[0]['nums']
                     ]);
                 if($up == 1){
-                    $key = 'bjk3:issue';
+                    $key = 'ahk3:issue';
                     Redis::set($key,$html[0]['issue']);
                 }
             } catch (\Exception $exception){
