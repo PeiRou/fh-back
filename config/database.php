1ba9004fcc590cf51055c8cb78e5c9fa9203fd66
vincent-lee <laomumeme@gmail.com>
2018-08-09 18:04:37 +0800

    zoe kill

--- config/database.php
+++ config/database.php
@@ -61,11 +61,11 @@ return [
             //'write' => [
             //    'host' => 'rm-j6cu60797usb9qsdz.mysql.rds.aliyuncs.com'
             //],
-            'host' => 'rm-j6cu60797usb9qsdzrw.mysql.rds.aliyuncs.com',
-            'port' => 3306,
-            'database' => 'fh-db',
-            'username' => 'laravel',
-            'password' => 'ab30317!',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
