9dfb9ff83722c772fbd6e0c42e0f70b298342bf8
vincent-lee <laomumeme@gmail.com>
2018-08-18 17:27:51 +0800

    zoe up new v

--- app/Http/Controllers/Back/SrcMemberController.php
+++ app/Http/Controllers/Back/SrcMemberController.php
@@ -376,14 +376,15 @@ class SrcMemberController extends Controller
         $password = $request->input('password');
         $fundPwd = $request->input('fundPwd');
         $bank_id = $request->input('bank');
-        if(!empty($bank_id))
+        if(!empty($bank_id)){
             $bank = DB::table('bank')->select('name')->where('bank_id',$bank_id)->first();
-        else
+            $bank = $bank->name;
+        }else
             $bank = '';
         $data = collect([
             'status'=>$request->input('status'),
             'bank_id'=>$bank_id,
-            'bank_name'=>$bank->name,
+            'bank_name'=>$bank,
             'bank_num'=>$request->input('bank_num'),
             'bank_addr'=>$request->input('bank_addr'),
             'mobile'=>$request->input('mobile'),
