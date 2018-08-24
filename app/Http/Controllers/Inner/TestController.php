1f100a2ae0497b7721aa15a81ee1b03d486959db
vincent-lee <laomumeme@gmail.com>
2018-08-24 23:13:01 +0800

    add zfdt

--- app/Http/Controllers/Inner/TestController.php
+++ app/Http/Controllers/Inner/TestController.php
@@ -12,9 +12,9 @@ class TestController extends Controller
         $zx_plays = ['鼠'=>3729,'牛'=>3730,'虎'=>3731,'兔'=>3732,'龙'=>3733,'蛇'=>3734,'马'=>3735,'羊'=>3736,'猴'=>3737,'鸡'=>3738,'狗'=>3739,'猪'=>3740];
         $openSX = ['鼠','虎','龙','蛇','羊','鸡'];
         $countOpen = array_count_values($openSX);
-        foreach ($countOpen as $item){
+        foreach ($countOpen as $kk => $vv){
             foreach ($zx_plays as $k => $v){
-                if ($item == $k){
+                if ($kk == $k){
                     echo $v.'</br>';
                 }
             }
