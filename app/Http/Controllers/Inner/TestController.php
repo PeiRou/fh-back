7ad69f920e37ea9ce1f6a5d0a5cd539fe6840163
vincent-lee <laomumeme@gmail.com>
2018-08-25 01:21:43 +0800

    add zfdt

--- app/Http/Controllers/Inner/TestController.php
+++ app/Http/Controllers/Inner/TestController.php
@@ -45,7 +45,9 @@ class TestController extends Controller
 
                 for($b = $key + 2; $b < count($a); $b++)
                 {
-                    echo $a[count($a)-1];
+                    if($a[$b] == $a[count($a)-1]){
+                        break;
+                    }
                     if($value !== $a[$i] && $a[$i] !== $a[$b]){
                         $key_01[] = [$value,$a[$i],$a[$b]];
                     }
