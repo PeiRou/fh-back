330c02aa36622c5dc023dcc7607825f59ab9194f
vincent-lee <laomumeme@gmail.com>
2018-08-25 01:18:57 +0800

    add zfdt

--- app/Http/Controllers/Inner/TestController.php
+++ app/Http/Controllers/Inner/TestController.php
@@ -45,9 +45,11 @@ class TestController extends Controller
 
                 for($b = $key + 2; $b < count($a); $b++)
                 {
+                    if($value==$value[count($a)-1]){
+                        break;
+                    }
                     if($value !== $a[$i] && $a[$i] !== $a[$b]){
-                        $kkk = [$value,$a[$i],$a[$b]];
-                        $key_01[] = asort($kkk);
+                        $key_01[] = [$value,$a[$i],$a[$b]];
                     }
                 }
             }
