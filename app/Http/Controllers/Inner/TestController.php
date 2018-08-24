1127a93ab4286112f31cf27a3507394db1f055c5
vincent-lee <laomumeme@gmail.com>
2018-08-25 01:43:55 +0800

    add zfdt

--- app/Http/Controllers/Inner/TestController.php
+++ app/Http/Controllers/Inner/TestController.php
@@ -35,7 +35,7 @@ class TestController extends Controller
 //        //print_r($arr);
 //        return $temp;
 
-        $a = [1,2,3,4];
+        $a = [1,2,3,4,5,6,7,8,9,10];
 
         foreach($a as $key => $value)
         {
