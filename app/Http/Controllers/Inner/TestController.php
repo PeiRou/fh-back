d860512a075d10ddf274b45a271158d2ef3ab6c2
vincent-lee <laomumeme@gmail.com>
2018-08-25 00:53:16 +0800

    add zfdt

--- app/Http/Controllers/Inner/TestController.php
+++ app/Http/Controllers/Inner/TestController.php
@@ -9,30 +9,40 @@ class TestController extends Controller
 {
     public function lhc()
     {
-        $CombinList = ['A','B','C','D'];
-        $count = count($CombinList);
-        //echo "['A','B','C','D']</br>";
-        $arr = [];
-        $arr3 = [];
-        $temp = [];
-        for($i=0;$i<count($CombinList);$i++){
-            $sit = $i+1;
-            for($b=0;$b<=$sit-1;$b++){
-                if($CombinList[$i] !== $CombinList[$b]){
-                    $arr[] = [$CombinList[$i],$CombinList[$b]];
-                }
-                for($z=0;$z<=$sit-3;$z++){
-                    if($CombinList[$i] !== $CombinList[$b] && $CombinList[$b] !== $CombinList[$z]){
-                        $temp[] = $CombinList[$i].','.$CombinList[$b].','.$CombinList[$z];
-                    }
-                }
+//        $CombinList = ['A','B','C','D'];
+//        $count = count($CombinList);
+//        //echo "['A','B','C','D']</br>";
+//        $arr = [];
+//        $arr3 = [];
+//        $temp = [];
+//        for($i=0;$i<count($CombinList);$i++){
+//            $sit = $i+1;
+//            for($b=0;$b<=$sit-1;$b++){
+//                if($CombinList[$i] !== $CombinList[$b]){
+//                    $arr[] = [$CombinList[$i],$CombinList[$b]];
+//                }
+//                for($z=0;$z<=$sit-3;$z++){
+//                    if($CombinList[$i] !== $CombinList[$b] && $CombinList[$b] !== $CombinList[$z]){
+//                        $temp[] = $CombinList[$i].','.$CombinList[$b].','.$CombinList[$z];
+//                    }
+//                }
+//            }
+//        }
+//        $temp = array_unique($temp);
+//        foreach ($temp as $k => $v){
+//            $temp[$k] = explode(",",$v);   //再将拆开的数组重新组装
+//        }
+//        //print_r($arr);
+//        return $temp;
+
+        $a = [1,2,3,4,5];
+
+        foreach($a as $key => $value)
+        {
+            for($i = $key + 1; $i < count($a); $i++)
+            {
+                echo $value.",".$a[$i]."\n";
             }
         }
-        $temp = array_unique($temp);
-        foreach ($temp as $k => $v){
-            $temp[$k] = explode(",",$v);   //再将拆开的数组重新组装
-        }
-        //print_r($arr);
-        return $temp;
     }
 }
