4e29f09c5e73140cdba5c63e1195cdc50bb10153
vincent-lee <laomumeme@gmail.com>
2018-08-24 22:41:58 +0800

    add zfdt

--- app/Http/Controllers/Inner/TestController.php
+++ app/Http/Controllers/Inner/TestController.php
@@ -11,6 +11,17 @@ class TestController extends Controller
     {
         $open = ['鼠','羊','羊','马','兔','虎'];
         $countOpen = array_count_values($open);
-        return $countOpen;
+        echo "鼠：".$countOpen['鼠'];
+        echo "牛：".$countOpen['牛'];
+        echo "虎：".$countOpen['虎'];
+        echo "兔：".$countOpen['兔'];
+        echo "龙：".$countOpen['龙'];
+        echo "蛇：".$countOpen['蛇'];
+        echo "马：".$countOpen['马'];
+        echo "羊：".$countOpen['羊'];
+        echo "猴：".$countOpen['猴'];
+        echo "鸡：".$countOpen['鸡'];
+        echo "狗：".$countOpen['狗'];
+        echo "猪：".$countOpen['猪'];
     }
 }
