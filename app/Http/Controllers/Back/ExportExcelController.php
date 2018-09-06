780b4e7986259dca9d73117caf87fd5dbc622e2f
laomumeme <laomumeme@gmail.com>
2018-09-06 19:52:58 +0800

    modify update

--- app/Http/Controllers/Back/ExportExcelController.php
+++ app/Http/Controllers/Back/ExportExcelController.php
@@ -16,43 +16,29 @@ class ExportExcelController extends Controller
         $rechargesType = $request->get('rechargesType');
 
         $cellData = [
-            ['订单日期','处理日期','会员','余额','订单号','付款方式','交易金额','操作人','收款信息','入款信息','状态'],
+//            ['订单日期','处理日期','会员','余额','订单号','付款方式','交易金额','操作人','收款信息','入款信息','状态'],
+            ['会员','交易金额','操作人','收款信息','状态'],
         ];
-        $exportRecharges = DB::table('recharges')->where('payType',$rechargesType)->whereBetween('created_at',[$startTime.' 00:00:00',$endTime.' 23:59:59'])->get();
+        $exportRecharges = DB::table('recharges')
+            ->leftJoin('users','recharges.userId','=','users.id')
+            ->select('users.id as user_id')
+            ->where('recharges.payType',$rechargesType)
+            ->whereBetween('recharges.created_at',[$startTime.' 00:00:00',$endTime.' 23:59:59'])
+            ->get();
         foreach ($exportRecharges as $item){
             $cellData[] = [
-                $item->created_at,
-                $item->process_date,
-                $item->userId,
-                $item->balance,
-                $item->orderNum,
-                $item->payType,
-                $item->amount,
-                $item->operation_account,
-                $item->shou_info,
-                $item->ru_info,
-                $item->status
+                $item->user_id,
+                $item->user_id,
+                $item->user_id,
+                $item->user_id,
+                $item->user_id,
             ];
         }
-
-        //return $exportData;
-
-//        $cellData = [
-//            ['订单日期','处理日期','会员','余额','订单号','付款方式','交易金额','操作人','收款信息','入款信息','状态'],
-//            ['1','2','3','4','5','6','7','8','9','10','11'],
-//            ['1','2','3','4','5','6','7','8','9','10','11'],
-//            ['1','2','3','4','5','6','7','8','9','10','11'],
-//            ['1','2','3','4','5','6','7','8','9','10','11'],
-//            ['1','2','3','4','5','6','7','8','9','10','11'],
-//        ];
-        Excel::create('111',function ($excel) use ($cellData){
-            $excel->sheet('123', function($sheet) use ($cellData){
-                $sheet->rows($cellData);
-            });
-        })->export('xls');
-
-//        return response()->json([
-//            'status' => true
-//        ]);
+        return $cellData;
+//        Excel::create('111',function ($excel) use ($cellData){
+//            $excel->sheet('123', function($sheet) use ($cellData){
+//                $sheet->rows($cellData);
+//            });
+//        })->export('xls');
     }
 }
