0f3d74d41a3c1c63a6c791368f228815c2e99047
laomumeme <laomumeme@gmail.com>
2018-09-06 17:25:21 +0800

    modify update

--- app/Http/Controllers/Back/ExportExcelController.php
+++ app/Http/Controllers/Back/ExportExcelController.php
@@ -15,9 +15,25 @@ class ExportExcelController extends Controller
         $endTime = $request->get('endTime');
         $rechargesType = $request->get('rechargesType');
 
+        $exportData = [];
         $exportRecharges = DB::table('recharges')->where('payType',$rechargesType)->whereBetween('created_at',[$startTime.' 00:00:00',$endTime.' 23:59:59'])->get();
+        foreach ($exportRecharges as $item){
+            $exportData[] = [
+                $item->created_at,
+                $item->process_date,
+                $item->userId,
+                $item->balance,
+                $item->orderNum,
+                $item->payType,
+                $item->amount,
+                $item->operation_account,
+                $item->shou_info,
+                $item->ru_info,
+                $item->status
+            ];
+        }
 
-        return $exportRecharges;
+        return $exportData;
 
 //        $cellData = [
 //            ['订单日期','处理日期','会员','余额','订单号','付款方式','交易金额','操作人','收款信息','入款信息','状态'],
