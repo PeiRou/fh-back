69fcbb05f6cbdaa48a492ac6dc537a84c56586f1
laomumeme <laomumeme@gmail.com>
2018-09-08 14:05:56 +0800

    modify update

--- public/back/js/pages/recharge_record.js
+++ public/back/js/pages/recharge_record.js
@@ -476,3 +476,34 @@ function excelRecharges() {
     }
 
 }
+
+function editLevels(uid,nowLevel) {
+    jc = $.confirm({
+        theme: 'material',
+        title: '修改会员层级',
+        closeIcon:true,
+        boxWidth:'20%',
+        content: 'url:/back/modal/editUserLevels/'+uid+'/'+nowLevel,
+        buttons: {
+            formSubmit: {
+                text:'确定提交',
+                btnClass: 'btn-blue',
+                action: function () {
+                    var form = this.$content.find('#editUserLevelsForm').data('formValidation').validate().isValid();
+                    if(!form){
+                        return false;
+                    }
+                    return false;
+                }
+            }
+        },
+        contentLoaded: function(data, status, xhr){
+            $('.jconfirm-content').css('overflow','hidden');
+            if(data.status == 403)
+            {
+                this.setContent('<div class="modal-error"><span class="error403">403</span><br><span>您无权进行此操作</span></div>');
+                $('.jconfirm-buttons').hide();
+            }
+        }
+    });
+}
