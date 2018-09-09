78258f3f736d2fe3c3bf56061718d88030dca82c
laomumeme <laomumeme@gmail.com>
2018-09-09 21:57:20 +0800

    modify kill excel

--- app/Http/Controllers/Bet/New_Msssc.php
+++ app/Http/Controllers/Bet/New_Msssc.php
@@ -98,7 +98,7 @@ class New_Msssc
                 $tmp = DB::connection('mysql::write')->select("SELECT sum(case when bunko >0 then bunko-bet_money else bunko end) as sumBunko FROM excel_bet WHERE issue = '{$issue}' and game_id = '{$gameId}'");
                 foreach ($tmp as&$value)
                     $excBunko = $value->sumBunko;
-                \Log::info('秒速时时彩 :'.$excBunko);
+                \Log::info('秒速时时彩 : '.$openCode.' => '.$excBunko);
                 $dataExcGame['game_id'] = $gameId;
                 $dataExcGame['issue'] = $issue;
                 $dataExcGame['opennum'] = $openCode;
