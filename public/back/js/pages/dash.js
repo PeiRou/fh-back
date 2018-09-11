1f6d30920ec017ec5e7fda2094212eb6f51d2dec
laomumeme <laomumeme@gmail.com>
2018-09-11 12:06:17 +0800

    modify error

--- public/back/js/pages/dash.js
+++ public/back/js/pages/dash.js
@@ -1,6 +1,6 @@
 $(function () {
     $.ajax({
-        url:'https://raw.githubusercontent.com/wx-games/wx_update/master/platform_notice.json?token=Ao9LFTXiKf1db9b8y90e0tDv0XUeqQmdks5boHfUwA%3D%3D',
+        url:'https://raw.githubusercontent.com/wx-games/wx_update/master/platform_notice.json',
         type:'get',
         success:function (result) {
             console.log(result);
