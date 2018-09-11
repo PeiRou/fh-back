135eea4ebc911967e601d39bb7337a3b1137a127
laomumeme <laomumeme@gmail.com>
2018-09-11 12:02:32 +0800

    modify error

--- public/back/js/pages/dash.js
+++ public/back/js/pages/dash.js
@@ -1,8 +1,7 @@
 $(function () {
     $.ajax({
         url:'https://raw.githubusercontent.com/wx-games/wx_update/master/platform_notice.json?token=Ao9LFTXiKf1db9b8y90e0tDv0XUeqQmdks5boHfUwA%3D%3D',
-        type:'OPTIONS',
-        dataType:'json',
+        type:'get',
         success:function (result) {
             console.log(result);
         }
