7dce02e5d2717ef871996f3fd7849a2a9a368c74
laomumeme <laomumeme@gmail.com>
2018-09-11 12:07:01 +0800

    modify error

--- public/back/js/pages/dash.js
+++ public/back/js/pages/dash.js
@@ -1,9 +1,3 @@
 $(function () {
-    $.ajax({
-        url:'https://raw.githubusercontent.com/wx-games/wx_update/master/platform_notice.json',
-        type:'get',
-        success:function (result) {
-            console.log(result);
-        }
-    })
+
 });
