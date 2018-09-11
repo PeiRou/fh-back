175784714220f7002305032f8665b2ec48f0848e
laomumeme <laomumeme@gmail.com>
2018-09-11 12:00:28 +0800

    modify error

--- public/back/js/pages/dash.js
+++ public/back/js/pages/dash.js
@@ -1,7 +1,7 @@
 $(function () {
     $.ajax({
         url:'https://raw.githubusercontent.com/wx-games/wx_update/master/platform_notice.json?token=Ao9LFTXiKf1db9b8y90e0tDv0XUeqQmdks5boHfUwA%3D%3D',
-        type:'get',
+        type:'OPTIONS',
         dataType:'json',
         success:function (result) {
             console.log(result);
