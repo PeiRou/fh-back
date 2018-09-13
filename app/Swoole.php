6acd825badd4ff1019f9d08ff046015bb5d54c47
“Zoe” <phapha0513@gmail.com>
2018-09-13 18:16:47 +0800

    modify

--- app/Swoole.php
+++ app/Swoole.php
@@ -5,19 +5,21 @@ namespace App;
 
 class Swoole
 {
+    private $timeout = 30;
     public function swooletest($type,$room)
     {
         $param['type'] = $type;
         $param['room'] = $room;
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, env('WS_CURL',"http://127.0.0.1").":".env('WS_PORT',9500));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
+        return $this->postSwoole($param);
+    }
+    private function postSwoole($param){
+        $this->ch = curl_init();
         //设置post数据
-        $post_data = $param;
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-        curl_exec($ch);
-        curl_close($ch);
+        curl_setopt($this->ch,CURLOPT_URL,env('WS_CURL',"http://127.0.0.1")."/dows/?type=".$param['type']."&room=".$param['room']);
+        curl_setopt($this->ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
+        curl_setopt($this->ch,CURLOPT_HEADER,0);
+        $output = curl_exec($this->ch);
+        return $output;
     }
 }
