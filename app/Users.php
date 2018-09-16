0179510f4bf2223210fc08817582bd7b269aa699
ashen <2594933065@qq.com>
2018-09-16 17:09:42 +0800

    会员回访和导出数据修改

--- app/Users.php
+++ app/Users.php
@@ -11,7 +11,10 @@ class Users extends Model
     protected $primaryKey = 'id';
 
     public static function exportUserData($aParam){
-        $aSql = "SELECT `users`.`bank_name`,`users`.`content`,`users`.`bank_num`,`users`.`bank_addr`,`users`.`wechat`,`users`.`username`,`users`.`fullName`,`users`.`email`,`users`.`mobile`,`users`.`created_at`,`users`.`saveMoneyCount`,`users`.`lastLoginTime`,`re`.`sumAmount` FROM `users` LEFT JOIN (SELECT (CASE WHEN SUM(amount) IS NULL THEN 0 ELSE SUM(amount) END) AS sumAmount,`userId` FROM `recharges` WHERE `status` = 3 GROUP BY `userId`) AS `re` ON `re`.`userId` = `users`.`id` WHERE `users`.`testFlag` = 0 ";
+        $aSql = "SELECT `users`.`bank_name`,`users`.`content`,`users`.`bank_num`,`users`.`bank_addr`,`users`.`wechat`,`users`.`username`,`users`.`fullName`,`users`.`email`,`users`.`mobile`,`users`.`created_at`,`users`.`saveMoneyCount`,`users`.`lastLoginTime`,`re`.`sumReAmount`,`dr`.`sumDrAmount` FROM `users` 
+LEFT JOIN (SELECT (CASE WHEN SUM(amount) IS NULL THEN 0 ELSE SUM(amount) END) AS sumReAmount,`userId` FROM `recharges` WHERE `status` = 2 AND `payType` != 'adminAddMoney' GROUP BY `userId`) AS `re` ON `re`.`userId` = `users`.`id` 
+LEFT JOIN (SELECT (CASE WHEN SUM(amount) IS NULL THEN 0 ELSE SUM(amount) END) AS sumDrAmount,`user_id` FROM `drawing` WHERE `status` = 2 GROUP BY `user_id`) AS `dr` ON `dr`.`user_id` = `users`.`id` 
+WHERE `users`.`testFlag` = 0 ";
         $aArray = [];
         if(isset($aParam['login_type']) && array_key_exists('login_type',$aParam)){
             if($aParam['login_type'] == 1){
