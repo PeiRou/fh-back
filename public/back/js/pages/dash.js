da39fb8ff0312a17230879b615e3f6ed855ef124
laomumeme <laomumeme@gmail.com>
2018-09-21 16:23:08 +0800

    1

--- public/back/js/pages/dash.js
+++ public/back/js/pages/dash.js
@@ -1,3 +1,10 @@
 $(function () {
-
+    $.ajax({
+        url:'http://info.platform.wuxianplay.com/dash_info.json',
+        dataType:'json',
+        type:'get',
+        success:function (result) {
+            console.log(result);
+        }
+    })
 });
