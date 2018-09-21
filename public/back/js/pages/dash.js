7de6bbb0ba06a79ead163b80f5abe87ea145f7b2
laomumeme <laomumeme@gmail.com>
2018-09-21 17:01:21 +0800

    1

--- public/back/js/pages/dash.js
+++ public/back/js/pages/dash.js
@@ -1,12 +1,11 @@
 $(function () {
     var url = 'https://info.platform.wuxianplay.com/dash_info.json';
-    $.jsonp({
-        "url": url,
-        "success": function(data) {
+    $.ajax({
+        url:url,
+        dataType:"jsonp",
+        jsonpCallback:"callback",
+        success:function(data){
             console.log(data);
-        },
-        "error": function(d,msg) {
-            alert("Could not find user "+msg);
         }
-    })
+    });
 });
