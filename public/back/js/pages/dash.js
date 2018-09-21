2cc0c51b92783f8d4cdbbfff1e252327d36927b3
laomumeme <laomumeme@gmail.com>
2018-09-21 17:09:56 +0800

    1

--- public/back/js/pages/dash.js
+++ public/back/js/pages/dash.js
@@ -2,7 +2,7 @@ $(function () {
     var url = 'https://info.platform.wuxianplay.com/dash_info.json';
     $.ajax({
         url:url,
-        dataType:"JSON",
+        dataType:"JSONP",
         success:function(data){
             console.log(data);
         }
