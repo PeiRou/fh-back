8669616dbe1a427316e7add9a7d91487b9753444
“Zoe” <phapha0513@gmail.com>
2018-10-03 17:44:27 +0800

    modify 活动每日报表的一页的笔数

--- public/back/js/pages/activityList.js
+++ public/back/js/pages/activityList.js
@@ -6,6 +6,7 @@ $(function () {
     $('#menu-activityManage-list').addClass('active');
 
     dataTable = $('#capitalDetailsTable').DataTable({
+        aLengthMenu: [[50]],
         searching: false,
         bLengthChange: false,
         processing: true,
