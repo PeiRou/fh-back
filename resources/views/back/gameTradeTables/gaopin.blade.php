3bcebc2206d53666852d182e8aabd569bbed9cbe
laomumeme <laomumeme@gmail.com>
2018-10-04 16:32:30 +0800

    1

--- resources/views/back/gameTradeTables/gaopin.blade.php
+++ resources/views/back/gameTradeTables/gaopin.blade.php
@@ -1,21 +1,30 @@
+<style>
+    .table-small-title{
+        word-break: break-all;
+        text-align: center;
+        color: #2d2d2d;
+        background: #f9f9f9;
+        font-weight: bold;
+    }
+    .table-title{
+        word-break: break-all;
+        text-align: center;
+        font-size: 16px;
+        background: #fdf1f1;
+        font-weight: bold;
+        color: red;
+    }
+</style>
 <table align="center" class="ui celled small table">
     <tbody>
     <tr class="firstRow">
-        <td width="190" align="center" style="word-break: break-all;">
-            玩法
-        </td>
-        <td width="190" align="center" style="word-break: break-all;">
-            单注下注最低金额
-        </td>
-        <td width="190" align="center" style="word-break: break-all;">
-            单注下注最高金额
-        </td>
-        <td width="190" align="center" style="word-break: break-all;">
-            当期下注最大
-        </td>
+        <td width="190" align="center" class="table-small-title">玩法</td>
+        <td width="190" align="center" class="table-small-title">单注下注最低金额</td>
+        <td width="190" align="center" class="table-small-title">单注下注最高金额</td>
+        <td width="190" align="center" class="table-small-title">当期下注最大</td>
     </tr>
     <tr>
-        <td align="center" rowspan="1" colspan="4" style="word-break: break-all;">
+        <td align="center" rowspan="1" colspan="4" class="table-title">
             北京赛车
         </td>
     </tr>
