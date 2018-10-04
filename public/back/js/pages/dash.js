b5312327cbf282046d91c6c9fd2860930291bf73
laomumeme <laomumeme@gmail.com>
2018-10-04 14:46:35 +0800

    1

--- public/back/js/pages/dash.js
+++ public/back/js/pages/dash.js
@@ -1,5 +1,6 @@
 $(function () {
     var url = 'https://info.platform.wuxianplay.com/dash_info.json';
+    var url_download = 'https://info.platform.wuxianplay.com/dash_download.json';
     $.ajax({
         url:url,
         dataType:"JSONP",
@@ -17,6 +18,25 @@ $(function () {
                 updateInfoText += "<a href='"+value.url+"' "+target+" class='dash_link'>【"+value.time+"】 "+value.title+"</a>"
             })
             $('#systemUpdateMessageBox').html(updateInfoText);
+        }
+    });
+    $.ajax({
+        url:url_download,
+        dataType:"JSONP",
+        jsonp:"callback",
+        jsonpCallback:"success_jsonpCallback",
+        success:function(data){
+            var downloadInfo = data.items;
+            var updateInfoText = "";
+            downloadInfo.forEach(function (value) {
+                if(value.url != "#"){
+                    var target = "target='_blank'";
+                } else {
+                    var target = "";
+                }
+                updateInfoText += "<a href='"+value.url+"' "+target+" class='dash_link'>【"+value.time+"】 "+value.title+"</a>"
+            })
+            $('#downloadMessageBox').html(updateInfoText);
         }
     });
 });
