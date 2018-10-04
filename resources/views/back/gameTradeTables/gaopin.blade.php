1d8b04cac1d4d7c37397c0917346234381b1eb6e
laomumeme <laomumeme@gmail.com>
2018-10-04 16:12:24 +0800

    1

--- resources/views/back/gameTradeTables/gaopin.blade.php
+++ resources/views/back/gameTradeTables/gaopin.blade.php
@@ -1,1 +1,53 @@
-www
+<table>
+    <tbody>
+    <tr class="firstRow">
+        <td width="190" valign="top" style="word-break: break-all;">
+            玩法
+        </td>
+        <td width="190" valign="top" style="word-break: break-all;">
+            单注下注最低金额<br/>
+        </td>
+        <td width="190" valign="top" style="word-break: break-all;">
+            单注下注最高金额<br/>
+        </td>
+        <td width="190" valign="top" style="word-break: break-all;">
+            当期下注最大
+        </td>
+    </tr>
+    <tr>
+        <td valign="top" rowspan="1" colspan="4" style="word-break: break-all;">
+            北京赛车<br/>
+        </td>
+    </tr>
+    <tr>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+    </tr>
+    <tr>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+    </tr>
+    <tr>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+    </tr>
+    <tr>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+    </tr>
+    <tr>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+        <td width="190" valign="top"></td>
+    </tr>
+    </tbody>
+</table>
