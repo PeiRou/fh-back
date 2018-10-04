a8ae870e946753f82a74c494716271f8d290ae82
laomumeme <laomumeme@gmail.com>
2018-10-04 15:46:11 +0800

    1

--- public/back/js/pages/game_trade.js
+++ public/back/js/pages/game_trade.js
@@ -1,4 +1,8 @@
 $(function () {
     $('#menu-gameManage').addClass('nav-show');
     $('#menu-gameManage-tradeSetting').addClass('active');
+
+    $('.menu .item').tab({
+        context: $('#context1')
+    });
 });
