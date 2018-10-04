5ce949cebdb0d7c6877157c2113c6b6309534895
laomumeme <laomumeme@gmail.com>
2018-10-04 16:28:20 +0800

    1

--- resources/views/back/gameTradeTables/gaopin.blade.php
+++ resources/views/back/gameTradeTables/gaopin.blade.php
@@ -1,22 +1,22 @@
 <table align="center" class="ui celled small table">
     <tbody>
     <tr class="firstRow">
-        <td width="190" valign="top" style="word-break: break-all;">
+        <td width="190" align="center" style="word-break: break-all;">
             玩法
         </td>
-        <td width="190" valign="top" style="word-break: break-all;">
-            单注下注最低金额<br/>
+        <td width="190" align="center" style="word-break: break-all;">
+            单注下注最低金额
         </td>
-        <td width="190" valign="top" style="word-break: break-all;">
-            单注下注最高金额<br/>
+        <td width="190" align="center" style="word-break: break-all;">
+            单注下注最高金额
         </td>
-        <td width="190" valign="top" style="word-break: break-all;">
+        <td width="190" align="center" style="word-break: break-all;">
             当期下注最大
         </td>
     </tr>
     <tr>
-        <td valign="top" rowspan="1" colspan="4" style="word-break: break-all;">
-            北京赛车<br/>
+        <td align="center" rowspan="1" colspan="4" style="word-break: break-all;">
+            北京赛车
         </td>
     </tr>
     <tr>
