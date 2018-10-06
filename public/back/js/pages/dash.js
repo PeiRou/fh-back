f8762eb2774f02def5b8c5adcb460e7962133cba
laomumeme <laomumeme@gmail.com>
2018-10-06 21:04:04 +0800

    1

--- public/back/js/pages/dash.js
+++ public/back/js/pages/dash.js
@@ -8,8 +8,10 @@ $(function () {
         success:function(data){
             var updateInfo = data.updateInfoItems;
             var downLoadInfo = data.downLoadItems;
+            var noticeInfo = data.noticeItems;
             var updateInfoText = "";
             var downLoadText = "";
+            var noticeText = "";
             updateInfo.forEach(function (value) {
                 if(value.url != "#"){
                     var target = "target='_blank'";
@@ -26,8 +28,17 @@ $(function () {
                 }
                 downLoadText += "<a href='"+value.url+"' "+target+" class='dash_link'>【"+value.time+"】 "+value.title+"</a>"
             });
+            noticeInfo.forEach(function (value) {
+                if(value.url != "#"){
+                    var target = "target='_blank'";
+                } else {
+                    var target = "";
+                }
+                noticeText = "<a href='"+value.url+"' "+target+" class='dash_link'>【"+value.time+"】 "+value.title+"</a>"
+            });
             $('#systemUpdateMessageBox').html(updateInfoText);
             $('#downloadMessageBox').html(downLoadText);
+            $('#noticeMessageBox').html(noticeText);
         }
     });
 });
