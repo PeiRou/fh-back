026c9ae5498e9ca85dc1eaa7912aaf2ac067c273
laomumeme <laomumeme@gmail.com>
2018-10-08 22:10:04 +0800

    1

--- app/Console/Commands/ISSUE_SEED_GDKLSF.php
+++ app/Console/Commands/ISSUE_SEED_GDKLSF.php
@@ -2,41 +2,48 @@
 
 namespace App\Console\Commands;
 
+use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class ISSUE_SEED_GDKLSF extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'command:name';
+    protected $signature = 'ISSUE_SEED_GDKLSF';
+    protected $description = '广东快乐十分-奖期';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
-        //
+        $curDate = date('Ymd');
+        $timeUp = date('Y-m-d 09:00:00');
+        $checkUpdate = DB::table('issue_seed')->where('id',1)->first();
+        $sql = "INSERT INTO game_gdklsf (issue,opentime) VALUES ";
+        for($i=1;$i<=84;$i++){
+            $timeUp = Carbon::parse($timeUp)->addMinutes(10);
+            if(strlen($i) == 1){
+                $i = '0'.$i;
+            }
+            $issue = $curDate.$i;
+            $sql .= "('$issue','$timeUp'),";
+        }
+        if($checkUpdate->gdklsf == $curDate){
+            \Log::info(date('Y-m-d').'广东快乐十分已存在');
+        } else {
+            $run = DB::statement(rtrim($sql, ',').";");
+            if($run == 1){
+                $update = DB::table('issue_seed')->where('id',1)->update([
+                    'gdklsf' => $curDate
+                ]);
+                if($update !== 1){
+                    \Log::info('广东快乐十分error');
+                }
+            } else {
+                \Log::info('广东快乐十分error');
+            }
+        }
     }
 }
--- app/Console/Commands/OPEN_FILE_GDKLSF.php
+++ app/Console/Commands/OPEN_FILE_GDKLSF.php
@@ -3,40 +3,30 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
 
 class OPEN_FILE_GDKLSF extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'command:name';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+    protected $signature = 'OPEN_FILE_GDKLSF';
     protected $description = 'Command description';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
-        //
+        $timeUp = date('09:00:00');
+        $str = "";
+        for($i=1;$i<=84;$i++){
+            $timeUp = Carbon::parse($timeUp)->addMinutes(10);
+            if(strlen($i) == 1){
+                $i = '0'.$i;
+            }
+            $str .= '"'.(string)$i.'":{"time":"'.$timeUp->toTimeString().'","issue":"'.(string)$i.'"},';
+        }
+        Storage::disk('gameTime')->put('gdklsf.json',"{".rtrim($str,',')."}");
     }
 }
