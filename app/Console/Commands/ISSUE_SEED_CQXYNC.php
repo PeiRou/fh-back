182dc3ca060033c588d3eac5c1efdccc00f37119
laomumeme <laomumeme@gmail.com>
2018-10-08 18:46:10 +0800

    1

---
+++ app/Console/Commands/ISSUE_SEED_CQXYNC.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ISSUE_SEED_CQXYNC extends Command
+{
+    protected $signature = 'ISSUE_SEED_CQXYNC';
+    protected $description = '重庆幸运农场-奖期';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $curDate = date('ymd');
+        $timeUp = date('Y-m-d 23:52:20');
+        $timeUp2 = date('Y-m-d 09:52:20');
+        $checkUpdate = DB::table('issue_seed')->where('id',1)->first();
+        $sql = "INSERT INTO game_cqxync (issue,opentime) VALUES ";
+        for($i=1;$i<=13;$i++){
+            $timeUp = Carbon::parse($timeUp)->addMinutes(10);
+            if(strlen($i) == 1){
+                $i = '00'.$i;
+            }
+            if(strlen($i) == 2){
+                $i = '0'.$i;
+            }
+            $issue = date('Ymd').$i;
+            $sql .= "('$issue','$timeUp'),";
+        }
+        for($i=1;$i<=84;$i++){
+            $timeUp2 = Carbon::parse($timeUp2)->addMinutes(10);
+            $num = 13 + $i;
+            if(strlen($num) == 2){
+                $num = '0'.$num;
+            }
+            $issue = date('Ymd').$num;
+            $sql .= "('$issue','$timeUp2'),";
+        }
+
+        if($checkUpdate->cqxync == $curDate){
+            \Log::info(date('Y-m-d').'重庆幸运农场期数已存在');
+        } else {
+            $run = DB::statement(rtrim($sql, ',').";");
+            if($run == 1){
+                $update = DB::table('issue_seed')->where('id',1)->update([
+                    'cqxync' => $curDate
+                ]);
+                if($update !== 1){
+                    \Log::info('重庆幸运农场error');
+                }
+            } else {
+                \Log::info('重庆幸运农场error');
+            }
+        }
+    }
+}
