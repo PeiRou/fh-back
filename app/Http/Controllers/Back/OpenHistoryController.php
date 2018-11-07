758cbae4aedbf9083c9f8f1b0040c09caa57c330
xiaocui <194198606@qq.com>
2018-11-07 12:47:19 +0800

    秒速赛车重新开奖修改牛牛号码

--- app/Http/Controllers/Back/OpenHistoryController.php
+++ app/Http/Controllers/Back/OpenHistoryController.php
@@ -1155,6 +1155,11 @@ class OpenHistoryController extends Controller
                 $opennum = implode(',',$this->exePCdd($number));
                 DB::table('game_' . Games::$aCodeGameName['pcdd'])->where('issue',$issue)->update(['is_open' => 1,'bunko' => 0,'opennum' => $opennum]);
             }
+            if($type == 'jspk10'){ //秒速赛车 修改牛牛
+                $niuniu = $this->exePK10nn($number);
+                $opennum =$this->nn($niuniu[0]).','.$this->nn($niuniu[1]).','.$this->nn($niuniu[2]).','.$this->nn($niuniu[3]).','.$this->nn($niuniu[4]).','.$this->nn($niuniu[5]);
+                DB::table('game_' . Games::$aCodeGameName[$type])->where('issue',$issue)->update(['niuniu' => $opennum]);
+            }
             if($type = 'bjpk10'){ //如果是北京pk10  修改牛牛的号码
                 $niuniu = $this->exePK10nn($number);
                 $opennum =$this->nn($niuniu[0]).','.$this->nn($niuniu[1]).','.$this->nn($niuniu[2]).','.$this->nn($niuniu[3]).','.$this->nn($niuniu[4]).','.$this->nn($niuniu[5]);
