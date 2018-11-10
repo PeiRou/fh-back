44f082210ecb7f15b1e25c560a4e104db1afbead
ashen <2594933065@qq.com>
2018-11-10 20:22:29 +0800

    修改返水明细

--- app/Http/Controllers/Back/Data/MembersDataController.php
+++ app/Http/Controllers/Back/Data/MembersDataController.php
@@ -299,7 +299,7 @@ JOIN `general_agent` ON `general_agent`.`ga_id` = `ag`.`gagent_id` ORDER BY `ag`
         })->where('agent_backwater.agent_id',$id);
         $capitalCount = $capitalModel->count();
         $capital = $capitalModel->select('agent_backwater.*','users.username','users.fullName','game.game_name')
-            ->join('users','users.id','=','agent_backwater.agent_id')->join('game','game.g_id','=','agent_backwater.game_id')
+            ->leftJoin('users','users.id','=','agent_backwater.agent_id')->leftJoin('game','game.g_id','=','agent_backwater.game_id')
             ->skip($aParam['start'])->take($aParam['length'])->get();
 
         $agentBackwaterStatus = AgentBackwater::$agentBackwaterStatus;
--- routes/web.php
+++ routes/web.php
@@ -525,7 +525,7 @@ Route::group(['middleware'=>['check-ip']],function () {
     Route::get('/back/modal/agentContent/{id}', 'Back\Ajax\ModalController@agentContent')->middleware('check-permission')->name('m.agent.viewDetails');
     Route::get('/back/modal/changeAgentMoney/{id}', 'Back\Ajax\ModalController@changeAgentMoney')->middleware('check-permission')->name('m.agent.editMoney');
     Route::get('/back/modal/agentCapitalHistory/{id}', 'Back\Ajax\ModalController@agentCapitalHistory')->middleware('check-permission')->name('m.agent.capitalDetails');
-    Route::get('/back/modal/agentBackwater/{id}', 'Back\Ajax\ModalController@agentBackwater')->middleware('check-permission')->name('m.agent.agentBackwater');
+    Route::get('/back/modal/agentBackwater/{id}', 'Back\Ajax\ModalController@agentBackwater')->middleware('check-permission')->name('m.agent.agentBackwater'); //查看代理返水
     Route::get('/back/modal/addBank', 'Back\Ajax\ModalController@addBank');
     Route::get('/back/modal/addUser', 'Back\Ajax\ModalController@addUser')->middleware('check-permission')->name('m.user.add');
     Route::get('/back/modal/userChangeAgent/{id}', 'Back\Ajax\ModalController@userChangeAgent')->middleware('check-permission')->name('m.user.changeAgent');
