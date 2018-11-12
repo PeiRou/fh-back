fdec62aaf84189faffd6b5f3b6200821520af4ea
laomumeme <laomumeme@gmail.com>
2018-11-12 20:12:17 +0800

    first commit

--- app/Console/Commands/new_qqffc.php
+++ app/Console/Commands/new_qqffc.php
@@ -9,17 +9,20 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Bet\Clong;
 
 class new_qqffc extends Command
 {
     protected  $code = 'qqffc';
     protected  $gameId = 113;
+    protected  $clong;
 
     protected $signature = 'new_qqffc';
     protected $description = '新-QQ分分彩';
 
-    public function __construct()
+    public function __construct(Clong $clong)
     {
+        $this->clong = $clong;
         parent::__construct();
     }
 
@@ -62,7 +65,11 @@ class new_qqffc extends Command
             //---kill start
             $opennum = $excel->kill_count($table,$res->expect,$this->gameId,$res->opencode);
             //---kill end
-            $opencode = empty($opennum)?$res->opencode:$opennum;
+            $opencode = empty($opennum)?$res->opencode:$opennum;//清除昨天长龙，在录第一期的时候清掉
+            if($filtered['issue']=='0001'){
+                DB::table('clong_kaijian1')->where('lotteryid',$this->gameId)->delete();
+                DB::table('clong_kaijian2')->where('lotteryid',$this->gameId)->delete();
+            }
             try{
                 DB::table('game_qqffc')->where('issue',$res->expect)->update([
                     'is_open' => 1,
@@ -71,6 +78,8 @@ class new_qqffc extends Command
                     'day'=>  date('d'),
                     'opennum'=> $opencode
                 ]);
+                $this->clong->setKaijian('qqffc',1,$opencode);
+                $this->clong->setKaijian('qqffc',2,$opencode);
             } catch (\Exception $exception){
                 \Log::info(__CLASS__ . '->' . __FUNCTION__ . ' Line:' . $exception->getLine() . ' ' . $exception->getMessage());
             }
