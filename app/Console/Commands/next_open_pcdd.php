28632a769962da68dc1eb42aae31cf08fac74157
laomumeme <laomumeme@gmail.com>
2018-11-25 01:28:18 +0800

    add New get guan number

--- app/Console/Commands/next_open_pcdd.php
+++ app/Console/Commands/next_open_pcdd.php
@@ -9,24 +9,24 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
 use App\Http\Controllers\Bet\Clong;
 
-class next_open_pk10 extends Command
+class next_open_pcdd extends Command
 {
-    protected  $code = 'bjpk10';
-    protected  $gameId = 50;
+    protected  $code = 'pcdd';
+    protected  $gameId = 66;
     protected  $clong;
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'next_open_pk10';
+    protected $signature = 'next_open_pcdd';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = '北京赛车-定時開號';
+    protected $description = 'PC蛋蛋-定時開號';
 
     /**
      * Create a new command instance.
@@ -46,47 +46,46 @@ class next_open_pk10 extends Command
      */
     public function handle()
     {
-        $table = 'game_bjpk10';
+        $table = 'game_pcdd';
 
         $redis = Redis::connection();
         $redis->select(0);
-        $redis_issue = $redis->get('pk10:issue');
-        $redis_needopen = $redis->exists('pk10:needopen')?$redis->get('pk10:needopen'):'';
-        $redis_next_issue = $redis->get('pk10:nextIssue');
+        $redis_issue = $redis->get('pcdd:issue');
+        $redis_needopen = $redis->exists('pcdd:needopen')?$redis->get('pcdd:needopen'):'';
+        $redis_next_issue = $redis->get('pcdd:nextIssue');
         if($redis_issue == ($redis_next_issue - 1) || $redis_needopen=='on')
             return 'no need';
         $excel = new Excel();
         $res = $excel->getNextIssue($table);
         //如果數據庫已經查不到需要追朔的獎期，則停止追朔
         if(empty($res)){
-            $redis->set('pk10:needopen','on');
+            $redis->set('pcdd:needopen','on');
             return 'Fail';
         }else{
-            $redis->set('pk10:needopen','');
+            $redis->set('pcdd:needopen','');
         }
         //當期獎期
         $nextIssue = $res->issue;
-        $openTime = $res->opentime;
+        $openTime = (string)$res->opentime;
 
         if($nextIssue == $redis_issue)
-            $url = Config::get('website.guanIssueServerUrl').'bjpk10';
+            $url = Config::get('website.guanIssueServerUrl').'pcdd';
         else
-            $url = Config::get('website.guanIssueServerUrl').'bjpk10?issue='.$nextIssue;
+            $url = Config::get('website.guanIssueServerUrl').'pcdd?issue='.$nextIssue;
         try {
             $html = json_decode(file_get_contents($url), true);
             //如果官方數據庫已經查不到需要追朔的獎期，則停止追朔
             if(!isset($html['issue'])){
-                $redis->set('pk10:needopen','on');
+                $redis->set('pcdd:needopen','on');
                 return 'no have';
             }
             //清除昨天长龙，在录第一期的时候清掉
-            if (substr($openTime,-8) == '09:07:30') {
-                DB::table('clong_kaijian1')->where('lotteryid', $this->gameId)->delete();
+            if (substr($openTime,-8) == '09:05:00') {
                 DB::table('clong_kaijian2')->where('lotteryid', $this->gameId)->delete();
             }
             if ($redis_issue !== $html['issue']) {
                 try {
-                    $up = DB::table('game_bjpk10')->where('issue', $html['issue'])
+                    $up = DB::table('game_pcdd')->where('issue', $html['issue'])
                         ->update([
                             'is_open' => 1,
                             'year' => date('Y'),
@@ -95,10 +94,9 @@ class next_open_pk10 extends Command
                             'opennum' => $html['nums']
                         ]);
                     if ($up == 1) {
-                        $key = 'pk10:issue';
+                        $key = 'pcdd:issue';
                         Redis::set($key, $html['issue']);
-                        $this->clong->setKaijian('pk10', 1, $html['nums']);
-                        $this->clong->setKaijian('pk10', 2, $html['nums']);
+                        $this->clong->setKaijian('pcdd', 2, $html['nums']);
                     }
                 } catch (\Exception $exception) {
                     \Log::info(__CLASS__ . '->' . __FUNCTION__ . ' Line:' . $exception->getLine() . ' ' . $exception->getMessage());
