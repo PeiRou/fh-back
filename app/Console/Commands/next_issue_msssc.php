b1101a1cde15f19512439fa31b5b195faae6c2b7
laomumeme <laomumeme@gmail.com>
2018-11-25 22:34:10 +0800

    add get guan number

---
+++ app/Console/Commands/next_issue_msssc.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Excel;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+
+class next_issue_msssc extends Command
+{
+    protected  $code = 'msssc';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'next_issue_msssc';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '秒速赛车+秒速牛牛-產下一期開盤';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $table = 'game_msssc';
+        $excel = new Excel();
+        $res = $excel->getNextBetIssue($table);
+        if(!$res)
+            return 'Fail';
+        $redis = Redis::connection();
+        $redis->select(0);
+        $beforeLotteryTime = $redis->get('msssc:nextIssueLotteryTime');
+        if($beforeLotteryTime>=time())
+            return 'no need';
+        //下一期獎期
+        $nextIssue = $res->issue;
+        $openTime = $res->opentime;
+
+        $nextIssueEndTime = Carbon::parse($openTime)->addSeconds(60)->toDateTimeString();
+        $nextIssueLotteryTime = Carbon::parse($openTime)->addSeconds(75)->toDateTimeString();
+
+        $New_nextIssue = $nextIssue+1;
+        if(substr($New_nextIssue,-4)=='1106'){
+            $nextDay = Carbon::parse($openTime)->addDay(1)->toDateTimeString();
+            $New_nextIssue = date("Ymd",strtotime($nextDay)).'0001';
+        }
+
+        $redis->set('msssc:nextIssue',(int)$New_nextIssue);
+        $redis->set('msssc:nextIssueLotteryTime',strtotime($nextIssueLotteryTime));
+        $redis->set('msssc:nextIssueEndTime',strtotime($nextIssueEndTime));
+        return 'Ok';
+    }
+}
