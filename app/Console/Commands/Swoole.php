d85070047da79433ab38f7838b6d3849f77ced63
laomumeme <laomumeme@gmail.com>
2018-11-26 23:06:30 +0800

    add swoole create multiple theade

---
+++ app/Console/Commands/Swoole.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class Swoole extends Command
+{
+    public $ws;
+    public $redis;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'swoole {action?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'swoole';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->redis = new \Redis();
+        $this->redis->connect(env('REDIS_HOST','127.0.0.0.1'), env('REDIS_PORT',6379));
+        $this->redis->select(1);
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $action = $this->argument('action');
+        switch ($action) {
+            case 'close':
+                break;
+            case 'clean':
+                $this->clean();
+                die;
+                break;
+            case 'start':
+                $this->init();
+            default:
+                $this->start();
+                break;
+        }
+    }
+
+    /***
+     * 初始化
+     */
+    private function init(){
+    }
+
+    /***
+     * 清空数据
+     */
+    private function clean(){
+    }
+
+    public function start(){
+        //创建websocket服务器对象，监听0.0.0.0:2021端口
+        $this->ws = new \swoole_websocket_server("0.0.0.0", 8881);
+
+        //监听WebSocket连接打开事件
+        $this->ws->on('open', function ($ws, $request) {
+        });
+
+        //监听WebSocket消息事件
+        $this->ws->on('message', function ($ws, $request) {
+        });
+        $this->ws->on('workerStart', function ($ws, $request) {
+
+        });
+        //接收WebSocket服务器推送功能
+        $this->ws->on('request', function ($serv) {
+            $serv->tick(1000, function ($id) {
+                var_dump($id);
+            });
+        });
+
+        //监听WebSocket连接关闭事件
+        $this->ws->on('close', function ($ws, $fd) {
+        });
+
+        $this->ws->start();
+    }
+}
