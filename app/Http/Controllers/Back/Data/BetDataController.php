5ea7c5dda69d47dc800dd46ffaa4fa129bf51bfd
ashen <2594933065@qq.com>
2018-11-29 13:02:24 +0800

    修改历史开奖

--- app/Http/Controllers/Back/Data/BetDataController.php
+++ app/Http/Controllers/Back/Data/BetDataController.php
@@ -241,10 +241,10 @@ class BetDataController extends Controller
                 ->where(function ($query) use ($status){
                     if(isset($status) && $status){
                         if($status == 'jiesuan'){
-                            $query->where("bet.bunko",'!=',0);
+                            $query->where("bet.bunko",'!=',0)->whereColumn("bet.bunko",'!=',"bet.bet_money");
                         }
                         if($status == '-8888'){
-                            $query->where("bet.bunko",'=','bet.bet_money');
+                            $query->whereColumn("bet.bunko",'=',"bet.bet_money");
                         }
                     }
                 })
