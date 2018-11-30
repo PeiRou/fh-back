09b691e87a22f2b396469143a7dc72efdf9cc32b
ashen <2594933065@qq.com>
2018-11-30 21:19:42 +0800

    总代，代理，会员报表添加查询说明

--- app/Http/Controllers/Back/SrcViewController.php
+++ app/Http/Controllers/Back/SrcViewController.php
@@ -1057,9 +1057,14 @@ class SrcViewController extends Controller
             ]);
         }
         if($date == 'month'){
+            $startTine = date('Y-m-d',mktime(0,0,0,date('m'),1,date('Y')));
+            $endTime = date('Y-m-d',mktime(23,59,59,date('m'),date('t'),date('Y')));
+            if($endTime < $startTine){
+                $endTime = $startTine;
+            }
             return response()->json([
-                'start'=> date('Y-m-d',mktime(0,0,0,date('m'),1,date('Y'))),
-                'end' => date('Y-m-d',mktime(23,59,59,date('m'),date('t'),date('Y')))
+                'start'=> $startTine,
+                'end' => $endTime
             ]);
         }
         if($date == 'month_ym'){
