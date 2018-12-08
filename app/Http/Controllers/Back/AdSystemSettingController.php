0583e1e5241e85fb8aead2a3b95df7de8562a925
zoe <zoe@gmail.com>
2018-12-08 22:33:13 +0800

    修复生成广告位报错

--- app/Http/Controllers/Back/AdSystemSettingController.php
+++ app/Http/Controllers/Back/AdSystemSettingController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Validator;
 
 class AdSystemSettingController extends Controller
