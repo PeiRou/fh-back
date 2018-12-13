8c2dfb58294b2db22a372942054a98a64f433434
“Zoe” <phapha0513@gmail.com>
2018-12-13 18:35:50 +0800

    add clear data cmd

---
+++ app/Console/Commands/clear_data.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
+
+class clear_issue_cache extends Command
+{
+
+    protected $signature = 'clear_data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '清除缓存';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $clearDate = date('Y-m-d 23:59:59',strtotime(date('Y-m-1 00:00:00',strtotime("-1 month")))-300);
+        $sql = "DELETE FROM bet WHERE created_at<='{$clearDate}' LIMIT 500";
+        $res = DB::connection('mysql::write')->statement($sql);
+        echo $res.PHP_EOL;
+        echo 'Ok';
+    }
+}
