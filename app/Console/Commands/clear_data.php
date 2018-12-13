c78358a87075faed76063c3b36ed0d83fa698276
“Zoe” <phapha0513@gmail.com>
2018-12-13 19:13:55 +0800

    add clear data cmd

--- app/Console/Commands/clear_data.php
+++ app/Console/Commands/clear_data.php
@@ -37,22 +37,26 @@ class clear_data extends Command
     public function handle()
     {
         $clearDate = date('Y-m-d 23:59:59',strtotime(date('Y-m-1 00:00:00',strtotime("-1 month")))-300);
-        //清投注表
+        //清-投注表
         $sql = "DELETE FROM bet WHERE created_at<='{$clearDate}' LIMIT 1000";
         $res = DB::connection('mysql::write')->statement($sql);
         echo 'table bet :'.$res.PHP_EOL;
-        //清秒速赛车
+        //清-秒速赛车
         $sql = "DELETE FROM game_mssc WHERE opentime<='{$clearDate}' LIMIT 1000";
         $res = DB::connection('mysql::write')->statement($sql);
         echo 'table game_mssc :'.$res.PHP_EOL;
-        //清秒速时时彩
+        //清-秒速时时彩
         $sql = "DELETE FROM game_msssc WHERE opentime<='{$clearDate}' LIMIT 1000";
         $res = DB::connection('mysql::write')->statement($sql);
         echo 'table game_msssc :'.$res.PHP_EOL;
-        //清秒速快三
+        //清-秒速快三
         $sql = "DELETE FROM game_msjsk3 WHERE opentime<='{$clearDate}' LIMIT 1000";
         $res = DB::connection('mysql::write')->statement($sql);
         echo 'table game_msjsk3 :'.$res.PHP_EOL;
+        //清-幸运六合彩
+        $sql = "DELETE FROM game_xylhc WHERE opentime<='{$clearDate}' LIMIT 1000";
+        $res = DB::connection('mysql::write')->statement($sql);
+        echo 'table game_xylhc :'.$res.PHP_EOL;
         echo 'Ok';
     }
 }
