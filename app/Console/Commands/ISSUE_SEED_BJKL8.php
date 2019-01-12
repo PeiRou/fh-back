0532f591e33a6e54a9428503fd64940ba6da3d1a
zoe <zoe@gmail.com>
2019-01-12 14:00:42 +0800

    modify create bjkl8 issue

--- app/Console/Commands/ISSUE_SEED_BJKL8.php
+++ app/Console/Commands/ISSUE_SEED_BJKL8.php
@@ -22,7 +22,7 @@ class ISSUE_SEED_BJKL8 extends Command
         $timeUp = date('Y-m-d 09:00:00');
         $checkUpdate = DB::table('issue_seed')->where('id',1)->first();
         $lastDay = Carbon::now()->addDay(-1)->toDateTimeString();
-        $checkLastIssue = DB::table('game_bjkl8')->where('is_open',1)->whereDate('opentime',date('Y-m-d',strtotime($lastDay)))->orderBy('issue','desc')->first();
+        $checkLastIssue = DB::table('game_bjkl8')->select(DB::raw('MAX(is) as maxid'),'issue')->whereDate('opentime',date('Y-m-d',strtotime($lastDay)))->first();
         $lastIssue = $checkLastIssue->issue;
 //        $lastIssue = '687326';
 
