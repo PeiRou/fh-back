c1c7a9589c4169db082fd96179cba445beee3f5f
zoe <zoe@gmail.com>
2019-01-13 10:03:40 +0800

    modify create bjpk10

--- app/Console/Commands/ISSUE_SEED_PK10.php
+++ app/Console/Commands/ISSUE_SEED_PK10.php
@@ -22,7 +22,7 @@ class ISSUE_SEED_PK10 extends Command
         $timeUp = date('Y-m-d 09:02:30');
         $checkUpdate = DB::table('issue_seed')->where('id',1)->first();
         $lastDay = Carbon::now()->addDay(-1)->toDateTimeString();
-        $checkLastIssue = DB::table('game_bjpk10')->select(DB::raw('MAX(id) as maxid'),'issue')->whereDate('opentime',date('Y-m-d',strtotime($lastDay)))->orderBy('issue','desc')->first();
+        $checkLastIssue = DB::table('game_bjpk10')->select(DB::raw('MAX(id) as maxid'),'issue')->where('opentime',date('Y-m-d 23:57:30',strtotime('-1 days')))->first();
         $lastIssue = $checkLastIssue->issue;
 //        $lastIssue = '687326';
 
