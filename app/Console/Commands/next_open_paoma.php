6bf6a1c0283751ce1d0dd9491527363c879c9740
zoe <zoe@gmail.com>
2019-01-16 10:30:29 +0800

    modify paoma open log

--- app/Console/Commands/next_open_paoma.php
+++ app/Console/Commands/next_open_paoma.php
@@ -109,11 +109,11 @@ class next_open_paoma extends Command
                     }
                 } catch (\Exception $exception) {
 //                    \Log::info(__CLASS__ . '->' . __FUNCTION__ . ' Line:' . $exception->getLine() . ' ' . $exception->getMessage());
-                    writeLog('game/paoma', __CLASS__ . '->' . __FUNCTION__ . ' Line:' . $exception->getLine() . ' ' . $exception->getMessage());
+                    writeLog('next_open', __CLASS__ . '->' . __FUNCTION__ . ' Line:' . $exception->getLine() . ' ' . $exception->getMessage());
                 }
             }
         } catch (\Exception $exception) {
-            writeLog('game/paoma', __CLASS__ . '->' . __FUNCTION__ . ' Line:' . $exception->getLine() . ' ' . $exception->getMessage());
+            writeLog('next_open', __CLASS__ . '->' . __FUNCTION__ . ' Line:' . $exception->getLine() . ' ' . $exception->getMessage());
 //            \Log::info(__CLASS__ . '->' . __FUNCTION__ . ' Line:' . $exception->getLine() . ' ' . $exception->getMessage());
         }
     }
