00596cda7b2b4612b31ff619c45c1a5b9ce83701
ashen <ashenonecjy@gmail.com>
2019-01-23 15:03:38 +0800

    1

--- app/Http/Controllers/Obtain/SendController.php
+++ app/Http/Controllers/Obtain/SendController.php
@@ -24,7 +24,6 @@ class SendController extends BaseController
         ];
         $result = CurlService::getInstance()->post(env('GENERAL_INTERFACE_URL').'/'.$url,
             $aArray);
-        var_dump($result);die();
         return json_decode($result,true);
     }
 
