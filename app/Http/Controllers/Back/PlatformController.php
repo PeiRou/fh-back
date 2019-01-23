15167bf9136210a76cdb9b3794e1469cf4f84bcf
ashen <ashenonecjy@gmail.com>
2019-01-23 19:46:15 +0800

    添加支付限制

--- app/Http/Controllers/Back/PlatformController.php
+++ app/Http/Controllers/Back/PlatformController.php
@@ -8,6 +8,7 @@ use App\SystemSetting;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Redis;
 
 class PlatformController extends Controller
 {
@@ -23,7 +24,24 @@ class PlatformController extends Controller
     public function pay(Request $request){
         $aParam = $request->input();
         $iOffer = Offer::where('id',$aParam['id'])->first();
+        if($iOffer->paystatus != 0){
+            return response()->json([
+                'status' => false,
+                'msg' => '该订单已支付,请刷新页面',
+            ]);
+        }
+        $redis = Redis::connection();
+        $redis->select(5);
+        $key = 'offer:'.$iOffer->order_id;
+        if($redis->exists($key)){
+            return response()->json([
+                'status' => false,
+                'msg' => '同一订单在10分钟内只能支付一次',
+            ]);
+        }
+        $redis->setex($key,600,time());
         $iOffer->order_no = 'O'.time().rand(10000,99999);
+        $iOffer->paystatus = 1;
         $iOffer->save();
         $aArray = [
             'platform_id' => SystemSetting::getValueByRemark1('payment_platform_id'),
