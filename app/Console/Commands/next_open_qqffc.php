bf1eae845e4fd8d26482e3219cb0287a60b777d0
“Zoe” <phapha0513@gmail.com>
2019-01-31 19:22:10 +0800

    add qqffc clong

--- app/Console/Commands/next_open_qqffc.php
+++ app/Console/Commands/next_open_qqffc.php
@@ -81,6 +81,11 @@ class next_open_qqffc extends Command
         //---kill end
         $opencode = empty($opennum)?$res->opencode:$opennum;
 
+        //清除昨天长龙，在录第一期的时候清掉
+        if($issuenum=='0001'){
+            DB::table('clong_kaijian1')->where('lotteryid',$this->gameId)->delete();
+            DB::table('clong_kaijian2')->where('lotteryid',$this->gameId)->delete();
+        }
         try {
             if ($redis_issue !== $needOpenIssue) {
                 try {
@@ -96,6 +101,8 @@ class next_open_qqffc extends Command
                         $key = 'qqffc:issue';
                         $redis->set($key, $needOpenIssue);
                         $redis->set('qqffc:gapnum',$gapnum);
+                        $this->clong->setKaijian('qqffc',1,$opencode);
+                        $this->clong->setKaijian('qqffc',2,$opencode);
                     }
                 } catch (\Exception $exception) {
                     writeLog('next_open', __CLASS__ . '->' . __FUNCTION__ . ' Line:' . $exception->getLine() . ' ' . $exception->getMessage());
