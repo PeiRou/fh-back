e7b3cd7dd8fffff8162e4c61e3fc495b919dce20
“Zoe” <phapha0513@gmail.com>
2019-02-11 23:46:13 +0800

    修改重庆时时彩投注跟封盘时间

--- app/Console/Commands/ISSUE_SEED_CQSSC.php
+++ app/Console/Commands/ISSUE_SEED_CQSSC.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 class ISSUE_SEED_CQSSC extends Command
 {
     protected $signature = 'ISSUE_SEED_CQSSC';
-    protected $description = '重庆时时彩期数生成-120';
+    protected $description = '重庆时时彩期数生成-59';
 
     public function __construct()
     {
@@ -19,13 +19,20 @@ class ISSUE_SEED_CQSSC extends Command
     public function handle()
     {
         $curDate = date('ymd');
-        $timeUp_Lingcheng = date('Y-m-d 00:00:00');
-        $timeUp_baitian = date('Y-m-d 09:50:00');
-        $timeUp_yejian = date('Y-m-d 21:55:00');
+        $timeUp_Lingcheng = date('Y-m-d 00:10:00');
+        $timeUp_baitian = date('Y-m-d 07:10:00');
         $checkUpdate = DB::table('issue_seed')->where('id',1)->first();
         $sql = "INSERT INTO game_cqssc (issue,opentime) VALUES ";
-        for($i=1;$i<=23;$i++){
-            $timeUp_Lingcheng = Carbon::parse($timeUp_Lingcheng)->addMinutes(5);
+        for($i=1;$i<=9;$i++){
+            $timeUp_Lingcheng = Carbon::parse($timeUp_Lingcheng)->addMinutes(20);
+            if(strlen($i) == 1){
+                $i = '00'.$i;
+            }
+            $issue = date('Ymd').$i;
+            $sql .= "('$issue','$timeUp_Lingcheng'),";
+        }
+        for($i=10;$i<=59;$i++){
+            $timeUp_baitian = Carbon::parse($timeUp_baitian)->addMinutes(10);
             if(strlen($i) == 1){
                 $i = '00'.$i;
             }
@@ -33,31 +40,10 @@ class ISSUE_SEED_CQSSC extends Command
                 $i = '0'.$i;
             }
             $issue = date('Ymd').$i;
-            $sql .= "('$issue','$timeUp_Lingcheng'),";
-        }
-        for($i=1;$i<=72;$i++){
-            $timeUp_baitian = Carbon::parse($timeUp_baitian)->addMinutes(10);
-            $num = 23 + $i;
-            if(strlen($num) == 2){
-                $num = '0'.$num;
-            }
-            $issue = date('Ymd').$num;
             $sql .= "('$issue','$timeUp_baitian'),";
         }
-        for($i=1;$i<=24;$i++){
-            $timeUp_yejian = Carbon::parse($timeUp_yejian)->addMinutes(5);
-            $num = 95 + $i;
-            if(strlen($num) == 2){
-                $num = '0'.$num;
-            }
-            $issue = date('Ymd').$num;
-            $sql .= "('$issue','$timeUp_yejian'),";
-        }
-        $lastIssue = date('Ymd').'120';
-        $lastDate = date('Y-m-d 23:59:59');
-        $sql .= "('$lastIssue','$lastDate'),";
         if($checkUpdate->cqssc == $curDate){
-            writeLog('ISSUE_SEED', date('Y-m-d').'期数已存在');
+            writeLog('ISSUE_SEED', date('Y-m-d').$this->description.'期数已存在');
         } else {
             $run = DB::statement(rtrim($sql, ',').";");
             if($run == 1){
