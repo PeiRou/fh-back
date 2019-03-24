84e628e5d885a2bcf7bc9ac1dcf14e9eb1665fea
ashen <ashenonecjy@gmail.com>
2019-03-25 00:33:38 +0800

    结算家redis锁

--- app/Console/Commands/BUNKO/BUNKO_msnn.php
+++ app/Console/Commands/BUNKO/BUNKO_msnn.php
@@ -23,19 +23,19 @@ class BUNKO_msnn extends Command
     {
         $table = 'game_mssc';
         $excel = new Excel();
-//        $get = $excel->stopBunko($this->gameId,60);
-//        if($get)
-//            return 'ing';
+        $get = $excel->stopBunko($this->gameId,60);
+        if($get)
+            return 'ing';
         $get = $excel->getNeedNNBunkoIssue($table);
         if($get){
-            $redis = Redis::connection();
-            $redis->select(0);
-            //阻止進行中
-            $key = 'Bunko:'.$this->gameId.'ing:'.$get->issue;
-            if($redis->exists($key)){
-                return 'ing';
-            }
-            $redis->setex($key,60,'ing');
+//            $redis = Redis::connection();
+//            $redis->select(0);
+//            //阻止進行中
+//            $key = 'Bunko:'.$this->gameId.'ing:'.$get->issue;
+//            if($redis->exists($key)){
+//                return 'ing';
+//            }
+//            $redis->setex($key,60,'ing');
 //        if ($get) {
             $update = DB::table($table)->where('id', $get->id)->update([
                 'nn_bunko' => 2
