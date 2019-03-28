751daf96f3dc7d418f2ac06eed35569a241cefd2
xiaocui <194198606@qq.com>
2019-03-28 14:46:07 +0800

    第三方游戏报表

---
+++ app/Repository/GamesApi/Card/TcReport.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * 棋牌报表处理
+ */
+
+namespace App\Repository\GamesApi\Card;
+
+use App\GamesApi;
+use Illuminate\Support\Facades\DB;
+
+
+class TcReport{
+    public $param;
+    private $iData = [];
+    private $res;
+    public $sqlArr;
+    public function __construct($aTime = null){
+        if(is_null($aTime))
+            $aTime = date('Y-m-d');
+        $this->param = (object)[
+            'aTime' => $aTime,
+            'startTime' => $aTime,
+            'endTime' => $aTime
+        ];
+    }
+
+    public function getRes(){
+        $GamesApi = new GamesApi();
+        $this->res = $GamesApi->tc_betInfoData($this->param);
+    }
+
+    public function createData()
+    {
+        foreach ($this->res as $k=>$v){
+            $this->iData[] = [
+                'bet_count' =>  $v->bet_count,
+                'user_count' =>  $v->user_count,
+                'AllBet' =>  $v->AllBet,
+                'Profit' =>  $v->Profit,
+                'validBetAmount' =>  $v->validBetAmount,
+                'productType' =>  $v->productType,
+                'upMoney' =>  $v->upMoney,
+                'downMoney' =>  $v->downMoney,
+                'validBetAmount' =>  $v->validBetAmount,
+                'date' =>  $this->param->aTime ?? date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
+            ];
+        }
+    }
+
+    public function insertData()
+    {
+        DB::table('report_tc')->where('date', $this->param->aTime)->delete();
+        DB::table('report_tc')->insert($this->iData);
+    }
+
+    public function __call($name, $arguments){
+        return call_user_func_array([$this,$name],$arguments);
+    }
+}
