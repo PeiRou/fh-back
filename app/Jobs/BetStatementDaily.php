30bc57f2b14d6805af26335d9fbedf81a45282c5
ashen <ashenonecjy@gmail.com>
2019-03-28 13:33:35 +0800

    投注报表修改

--- app/Jobs/BetStatementDaily.php
+++ app/Jobs/BetStatementDaily.php
@@ -40,7 +40,7 @@ class BetStatementDaily implements ShouldQueue
 
         try {
             //获取投注
-            if(strtotime($this->aDateTime) >= strtotime(date('Y-m-d',strtotime('-1 day'))))
+            if(strtotime($this->aDateTime) >= strtotime(date('Y-m-d')))
                 $aBet = Bets::getReportBet($this->aDateTime,$this->aDateTime.' 23:59:59');
             else
                 $aBet = BetHis::getReportBet($this->aDateTime,$this->aDateTime.' 23:59:59');
