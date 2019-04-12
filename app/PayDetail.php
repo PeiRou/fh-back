0ac3b815222a07a59292448a77b944c21da0f449
ashen <ashenonecjy@gmail.com>
2019-04-12 15:06:22 +0800

    支付明细统计拉取数据

---
+++ app/PayDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class PayDetail extends Model
+{
+    protected $table = 'pay_detail';
+
+
+}
