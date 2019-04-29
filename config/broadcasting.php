aa08d97bb490f6df3ac1c232ed16b5982d77876c
zoe <zoe@gmail.com>
2019-04-29 17:33:36 +0800

    抑制PUSH找不到会报错

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -32,9 +32,9 @@ return [
 
         'pusher' => [
             'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
+            'key' => env('PUSHER_APP_KEY',''),
+            'secret' => env('PUSHER_APP_SECRET',''),
+            'app_id' => env('PUSHER_APP_ID',''),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
