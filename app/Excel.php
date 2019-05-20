51c370b1ee8b6288201b61036b1606db76861cff
zoe <zoe@gmail.com>
2019-05-20 21:58:57 +0800

    修复pusher报错导致无法往下继续执行

--- app/Excel.php
+++ app/Excel.php
@@ -158,15 +158,13 @@ class Excel
     }
     //中奖推送
     private function pushWinInfo($pushData){
-        writeLog('pusher','excel:'.json_encode($pushData));
+        $url = "http://127.0.0.1:9500?thread=PARAM_PUSH_WIN";
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "http://127.0.0.1:9500?thread=PARAM_PUSH_WIN");
+        curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_TIMEOUT, 1);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $pushData);
-        $tmp = curl_exec($curl);
+        curl_exec($curl);
         $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         $err = curl_errno($curl);
         curl_close($curl);
@@ -174,6 +172,22 @@ class Excel
             writeLog('pusher','res: no'.$err.'==='.$httpcode);
         else
             writeLog('pusher','res: yes');
+//        writeLog('pusher','excel:'.json_encode($pushData));
+//        $curl = curl_init();
+//        curl_setopt($curl, CURLOPT_URL, "http://127.0.0.1:9500?thread=PARAM_PUSH_WIN");
+//        curl_setopt($curl, CURLOPT_HEADER, 0);
+//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+//        curl_setopt($curl, CURLOPT_TIMEOUT, 1);
+//        curl_setopt($curl, CURLOPT_POST, true);
+//        curl_setopt($curl, CURLOPT_POSTFIELDS, $pushData);
+//        $tmp = curl_exec($curl);
+//        $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+//        $err = curl_errno($curl);
+//        curl_close($curl);
+//        if (($err) || (!in_array($httpcode,array(200,500))))
+//            writeLog('pusher','res: no'.$err.'==='.$httpcode);
+//        else
+//            writeLog('pusher','res: yes');
     }
     //反水
     public function reBackUser($gameId,$issue,$gameName=''){
