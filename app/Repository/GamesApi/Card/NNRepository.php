45d83bcddb618eaea80c9e37d186b544468157ee
xiaocui <194198606@qq.com>
2019-09-09 20:12:21 +0800

    nn棋牌使用第三方接口

--- app/Repository/GamesApi/Card/NNRepository.php
+++ app/Repository/GamesApi/Card/NNRepository.php
@@ -8,6 +8,9 @@ use Illuminate\Support\Facades\Redis;
 
 class NNRepository extends BaseRepository
 {
+
+    public $is_proxy_pass = true; //这个游戏是否使用代理那台服务器
+
     public function __construct($config){
         parent::__construct($config,'Utils');
     }
