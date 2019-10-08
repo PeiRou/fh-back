a63cde74f261a5f4d8dff61dac8e02471861e8a0
zoe <zoe@gmail.com>
2019-10-08 18:17:13 +0800

    修复结算报错问题

--- app/Console/Commands/Swoole.php
+++ app/Console/Commands/Swoole.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Excel;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
@@ -13,12 +12,22 @@ class Swoole extends Command
 {
     public $ws;
     public $serv;
-    public $num = [];
-    public $firstId = 0;
-    private $doOne = [
-        'CURL_PUSH_THREAD',
-        'BUNKO_msnn',
-        'BUNKO_pknn',
+    public $num;
+    public $maxId = 8;
+    public $threadArray= [
+        'BUNKO_1_bjkl8','BUNKO_1_pk10','BUNKO_1_cqssc','BUNKO_1_cqxync','BUNKO_1_gd11x5','BUNKO_1_gdklsf','BUNKO_1_gsk3',
+        'BUNKO_1_gxk3','BUNKO_1_gzk3','BUNKO_1_hbk3','BUNKO_1_hebeik3','BUNKO_1_jsk3','BUNKO_1_msft','BUNKO_1_msjsk3',
+        'BUNKO_1_qqffc','BUNKO_msnn','BUNKO_1_mssc','BUNKO_1_msssc','BUNKO_1_paoma','BUNKO_1_pcdd','BUNKO_pknn',
+        'BUNKO_1_xjssc','BUNKO_1_xylhc','BUNKO_1_kssc','BUNKO_1_ksft','BUNKO_1_ksssc','BUNKO_1_twxyft',
+        'BUNKO_1_sfsc','BUNKO_1_sfssc','BUNKO_1_jslhc','BUNKO_1_sflhc','BUNKO_1_xyft','BUNKO_1_ahk3',
+        'BUNKO_1_xykl8','BUNKO_1_xylsc','BUNKO_1_xylft','BUNKO_1_xylssc','BUNKO_1_xy28','BUNKO_1_twbgc',
+        'BUNKO_1_twbg28','BUNKO_1_hlsx','BUNKO_1_yfsc','BUNKO_1_yfssc','BUNKO_1_yflhc','BUNKO_1_efsc','BUNKO_1_efssc',
+        'BUNKO_1_eflhc','BUNKO_1_wfsc','BUNKO_1_wfssc','BUNKO_1_wflhc','BUNKO_1_shfsc','BUNKO_1_shfssc','BUNKO_1_shflhc',
+        'BUNKO_1_hkk3','BUNKO_1_yfk3','BUNKO_1_efk3','BUNKO_1_sfk3','BUNKO_1_wfk3','KILL_1_msft','KILL_1_mssc','KILL_1_msssc',
+        'KILL_1_paoma','KILL_1_xylhc','KILL_1_msjsk3','KILL_1_qqffc','KILL_1_kssc','KILL_1_ksft','KILL_1_ksssc','KILL_1_twxyft',
+        'KILL_1_sfsc','KILL_1_sfssc','KILL_1_jslhc','KILL_1_sflhc','KILL_1_xykl8','KILL_1_xylsc','KILL_1_xylft','KILL_1_xylssc',
+        'KILL_1_yfsc','KILL_1_yfssc','KILL_1_yflhc','KILL_1_efsc','KILL_1_efssc','KILL_1_eflhc','KILL_1_wfsc','KILL_1_wfssc',
+        'KILL_1_wflhc','KILL_1_shfsc','KILL_1_shfssc','KILL_1_shflhc','KILL_1_hkk3','KILL_1_yfk3','KILL_1_efk3','KILL_1_sfk3','KILL_1_wfk3'
     ];
     /**
      * The name and signature of the console command.
@@ -71,70 +80,12 @@ class Swoole extends Command
      * 初始化
      */
     private function init(){
-        sleep(3);
         $redis = Redis::connection();
         $redis->select(0);
         $redis->flushdb();        //清除所有开奖相关redis
-        $key = 'thread';
-        $files = Storage::disk($key)->files();
-        foreach ($files as $file){
-            if(Storage::disk($key)->exists($file))
-                Storage::disk($key)->delete($file);
+        if(Storage::disk('thread')->exists('thread')){
+            Storage::disk('thread')->delete('thread');
         }
-        $key = 'needbunko';
-        $files = Storage::disk($key)->files();
-        foreach ($files as $file){
-            if(Storage::disk($key)->exists($file))
-                Storage::disk($key)->delete($file);
-        }
-        $key = 'needkill';
-        $files = Storage::disk($key)->files();
-        foreach ($files as $file){
-            if(Storage::disk($key)->exists($file))
-                Storage::disk($key)->delete($file);
-        }
-//        $do['code'] = '';
-//        $this->timer = $this->serv->tick(1000, function($id) use ($do){
-//            $redis = Redis::connection();
-//            //设置ID计数器
-//            $this->setId($id);
-//            //开始计数器
-//            foreach ($this->doOne as $k => $v){
-//                echo $v.PHP_EOL;
-//                $do['thread'] = $v;
-//                try{
-//                    $this->settimer($id,$do, $redis);
-//                }catch (\exception $exception){
-//                    continue;
-//                }
-//            }
-//            echo $this->num[$id]['num'].PHP_EOL;
-//            $this->num[$id]['num'] ++;
-//            $redis->disconnect();
-//        });
-    }
-
-    /***
-     * 初始化
-     */
-    private function first(){
-        $do['code'] = '';
-        $this->timer = $this->serv->tick(1000, function($id) use ($do){
-            $redis = Redis::connection();
-            //开始计数器
-            foreach ($this->doOne as $k => $v){
-//                echo $v.PHP_EOL;
-                $do['thread'] = $v;
-                try{
-                    $this->settimer($id,$do, $redis,0);
-                }catch (\exception $exception){
-                    continue;
-                }
-            }
-
-            $this->firstId ++;
-            echo $this->firstId.PHP_EOL;
-        });
     }
 
     /***
@@ -154,131 +105,70 @@ class Swoole extends Command
         //监听WebSocket消息事件
         $this->ws->on('message', function ($ws, $request) {
         });
-        $this->ws->on('workerStart', function ($serv,$workid) {
+        $this->ws->on('workerStart', function ($serv) {
             $this->serv = $serv;
             $this->num = array();
-            if($workid===0)
-                $this->first();
         });
         $this->ws->on('request', function ($serv, $response) {
             $data['thread'] = isset($serv->post['thread'])?$serv->post['thread']:(isset($serv->get['thread'])?$serv->get['thread']:'');      //定时任务名称
             $data['thread2'] = isset($serv->post['thread2'])?$serv->post['thread2']:(isset($serv->get['thread2'])?$serv->get['thread2']:'');      //定时任务名称
             $data['code'] = '';
-            if($data['thread'] == 'GameApiGetBet' || isset($serv->get['GamesApiArtisan'])){
-                if(isset($serv->get['GamesApiArtisan'])) unset($serv->get['GamesApiArtisan']);
-                if(isset($serv->get['thread'])) unset($serv->get['thread']);
-                ob_start();
-                Artisan::call($data['thread'], $serv->get);
-                $response->end(ob_get_clean());
-                return '';
-            }else if(substr($data['thread'],0,9) == 'AgentOdds'){
-                $tmp = explode('-',$data['thread']);
-                $data['extra'] = ['code'=>$tmp[1],'issue'=>$tmp[2]];
-                $data['thread'] = $tmp[0];
-                $data['exethread'] = $tmp[0];
-                $this->exeComds($data);
-                return '';
-            }else if(substr($data['thread'],0,7) == 'BUNKO_1'){
-                $tmp = explode('_',$data['thread']);
-                $data['code'] = $tmp[2];
-                $data['exethread'] = 'BUNKO_1';
-                $data['key'] = 'needbunko';
-                $key = 'needbunko';
-                $filename = time().'-needbunko-'.$data['exethread'].'-'.$data['code'];
-                if(count(Storage::disk($key)->files())<200);
-                    Storage::disk($key)->put($filename,json_encode($data));
-                return '';
-            }else if(substr($data['thread'],0,6) == 'KILL_1'){
-                $tmp = explode('_',$data['thread']);
-                $data['code'] = $tmp[2];
-                $data['extra'] = ['code'=>$data['code']];
-                $data['exethread'] = 'KILL_1';
-                $data['key'] = 'needkill';
-                $key = 'needkill';
-                $filename = time().'-needkill-'.$data['exethread'].'-'.$data['code'];
-                if(count(Storage::disk($key)->files())<200);
-                    Storage::disk($key)->put($filename,json_encode($data));
-                return '';
-            }else if(substr($data['thread'],0,6) == 'CHECK_'){
-                switch ($data['thread']){
-                    case 'CHECK_BUNKO':
-                        $data['key'] = 'needbunko';
-                        break;
-                    case 'CHECK_KILL':
-                        $data['key'] = 'needkill';
-                        break;
+            if(empty($data['thread2'])){
+                if($data['thread'] == 'GameApiGetBet' || isset($serv->get['GamesApiArtisan'])){
+                    if(isset($serv->get['GamesApiArtisan'])) unset($serv->get['GamesApiArtisan']);
+                    if(isset($serv->get['thread'])) unset($serv->get['thread']);
+                    ob_start();
+                    Artisan::call($data['thread'], $serv->get);
+                    $response->end(ob_get_clean());
+                    return '';
+                }else if(substr($data['thread'],0,7) == 'BUNKO_1'){
+                    $tmp = explode('_',$data['thread']);
+                    $data['code'] = $tmp[2];
+                    $info['extra'] =  ['code'=>$data['code']];
+                    $data['thread'] = 'BUNKO_1';
+                }else if(substr($data['thread'],0,6) == 'KILL_1'){
+                    $tmp = explode('_',$data['thread']);
+                    $data['code'] = $tmp[2];
+                    $info['extra'] =  ['code'=>$data['code']];
+                    $data['thread'] = 'KILL_1';
+                }else if(substr($data['thread'],0,9) == 'AgentOdds'){
+                    $tmp = explode('-',$data['thread']);
+                    $data['extra'] = ['code'=>$tmp[1],'issue'=>$tmp[2]];
+                    $data['thread'] = $tmp[0];
+                    $data['exethread'] = $tmp[0];
+                    $this->exeComds($data);
+                    return '';
                 }
-                $this->timer = $this->serv->tick(1000, function($id) use ($data){
-                    $redis = Redis::connection();
-
-                    //设置ID计数器
-                    $this->setId($id);
-                    //开始计数器
-                    $key = $data['key'];
-//                    $key = 'needbunko';
-                    $files = Storage::disk($key)->files();
-                    $ii = 0;
-                    foreach ($files as $file){
-                        try{
-                            if(Storage::disk($key)->exists($file) && Storage::disk($key)->delete($file)) {
-                                $tmp = explode('-',$file);
-                                $info['thread'] =  $tmp[2];
-                                $info['exethread'] =  $tmp[2];
-                                $info['code'] =  $tmp[3];
-                                $info['extra'] =  ['code'=>$info['code']];
-                                $rsKey = 'Artisan:'.$info['thread'].'-'.$info['code'];
-    //                            echo 'do-1-'.$rsKey.PHP_EOL;
-                                if($redis->exists($rsKey))
-                                    continue;
-    //                            echo 'do-2-'.$rsKey.PHP_EOL;
-                                $this->settimer_new($id,$info, $redis);
-                                $ii++;
-                            }
-                        }catch (\exception $exception){
-                            continue;
-                        }
-                        if($ii>10)
-                            break;
+            }else if($data['thread2']=='push'){
+                $ii = 1;
+                foreach ($this->threadArray as $key => $val){
+                    if(substr($val,0,7) == 'BUNKO_1'){
+                        $tmp = explode('_',$val);
+                        $data['code'] = $tmp[2];
+                        $info['extra'] =  ['code'=>$data['code']];
+                        $data['thread'] = 'BUNKO_1';
+                    }else if(substr($val,0,6) == 'KILL_1'){
+                        $tmp = explode('_',$val);
+                        $data['code'] = $tmp[2];
+                        $info['extra'] =  ['code'=>$data['code']];
+                        $data['thread'] = 'KILL_1';
                     }
-                    $this->num[$id]['num'] ++;
-                    $redis->disconnect();
-                });
-                return false;
+                    $tmpIndex = ($ii%$this->maxId)+1;
+                    $this->num[$tmpIndex]['cmds'][$data['thread'].'-'.$data['code']] = $data;
+                    $ii++;
+                }
             }
-//            else if(substr($data['thread'],0,4) == 'DO_1'){
-//                echo 'dooo-1-0-'.json_encode($data).PHP_EOL;
-//                $this->timer = $this->serv->tick(1000, function($id) use ($data){
-//                    $redis = Redis::connection();
-//
-//                    //设置ID计数器
-//                    $this->setId($id);
-//                    //开始计数器
-//                    foreach ($this->doOne as $k => $v){
-//                        echo $v.PHP_EOL;
-//                        $do['thread'] = $v;
-//                        $do['code'] = '';
-//                        try{
-//                            $this->settimer($id,$do, $redis);
-//                        }catch (\exception $exception){
-//                            continue;
-//                        }
-//                    }
-//                    echo $this->num[$id]['num'].PHP_EOL;
-//                    $this->num[$id]['num'] ++;
-//                    $redis->disconnect();
-//                });
-//                return false;
-//            }
-
-
-            if(env('IS_CLOUD',0)==0){       //如果非云主机
-                DB::disconnect();
-                $this->exeComds($data);
-            }else{
+            $this->timer = $this->serv->tick(1000, function($id) use ($data){
+                $this->maxId = $id>$this->maxId?$id:$this->maxId;
                 $redis = Redis::connection();
-                $redis->select(0);
-                $this->cldComds($redis,$data);
-            }
+
+                //设置ID计数器
+                $this->setId($id);
+                //开始计数器
+                $this->settimer($id,$data, $redis);
+                $this->num[$id]['num'] ++;
+                $redis->disconnect();
+            });
         });
 
         //监听WebSocket连接关闭事件
@@ -288,7 +178,7 @@ class Swoole extends Command
         $this->ws->start();
     }
     //计数到60则停下
-    private function settimer_new($id,$data,$redis){
+    private function settimer($id,$data,$redis){
         if(!isset($data['thread']) || empty($data['thread']))
             $this->serv->clearTimer($id);
         try{
@@ -297,33 +187,18 @@ class Swoole extends Command
                 $this->exeComds($data);
             }else{
                 $redis->select(0);
-                $this->cldComds($redis,$data);
+                if(isset($this->num[$id]['cmds']))
+                    foreach ($this->num[$id]['cmds'] as $key => $val ){
+                        $this->cldComds($redis,$val,'cmds',$id);
+                    }
+                else
+                    $this->cldComds($redis,$data);
             }
         }catch (\exception $exception){
             writeLog('error',$exception->getFile(). '-> Line:' . $exception->getLine() . ' ' . $exception->getMessage());
             writeLog('error',$data);
         }
         if($this->num[$id]['num']>=59)
-            $this->serv->clearTimer($id);
-    }
-    //计数到60则停下
-    private function settimer($id,$data,$redis,$total_num=59){
-        if(!isset($data['thread']) || empty($data['thread']))
-            $this->serv->clearTimer($id);
-//        echo json_encode($data).PHP_EOL;
-        try{
-            if(env('IS_CLOUD',0)==0){       //如果非云主机
-                DB::disconnect();
-                $this->exeComds($data);
-            }else{
-                $redis->select(0);
-                $this->cldComds($redis,$data);
-            }
-        }catch (\exception $exception){
-            writeLog('error',$exception->getFile(). '-> Line:' . $exception->getLine() . ' ' . $exception->getMessage());
-            writeLog('error',$data);
-        }
-        if($total_num>0 && $this->num[$id]['num']>=$total_num)
             $this->serv->clearTimer($id);
     }
     private function setId($id){
@@ -337,7 +212,6 @@ class Swoole extends Command
             DB::disconnect();
             $this->exeComds($data);
             $redis->del($key);
-
         }
     }
     private function exeComds($data){
