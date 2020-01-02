26a0a2bb004187f498181afd5fabbb96b3336504
ashen <ashenonecjy@gmail.com>
2020-01-02 12:48:27 +0800

    注册报表修改

--- app/GeneralAgent.php
+++ app/GeneralAgent.php
@@ -13,4 +13,8 @@ class GeneralAgent extends Model
     public static function betGeneralReportData(){
         return self::select(DB::raw("`general_agent`.`account` AS `generalAccount`,`general_agent`.`ga_id` AS `generalId`,`general_agent`.`name` AS `generalName`"))->get();
     }
+
+    public static function getRegisteredCount($date){
+        return self::where('created_at','>=',$date)->where('created_at','<=',$date.' 23:59:59')->count();
+    }
 }
