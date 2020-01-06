40065bdba85299df18e0d2b1beb135e06889724a
xiaocui <194198606@qq.com>
2020-01-06 14:14:54 +0800

    会员报表增加中奖金额字段

--- app/BetHis.php
+++ app/BetHis.php
@@ -452,6 +452,7 @@ class BetHis extends Model
         $aSql = "SELECT LEFT(`updated_at`,10) AS `date`,`user_id`,COUNT(`bet_id`) AS `idCount`,SUM(`bet_money`) AS `betMoneySum`,
                   SUM(CASE WHEN `game_id` IN(90,91) THEN (CASE WHEN `nn_view_money` > 0 THEN `bet_money` ELSE 0 END) ELSE (CASE WHEN `bunko` >0 THEN `bet_money` ELSE 0 END) END) AS `sumWinbet`,
                   SUM(CASE WHEN `game_id` IN(90,91) THEN `nn_view_money` ELSE (CASE WHEN `bunko` >0 THEN `bunko` - `bet_money` ELSE `bunko` END) END) AS `sumBunko`,
+                  SUM(CASE WHEN `game_id` IN(90,91) THEN (CASE WHEN `nn_view_money` > 0 THEN `nn_view_money` ELSE 0 END) ELSE (CASE WHEN `bunko` >0 THEN `bunko` - `bet_money` ELSE 0 END) END) AS `sumBonus`,
                   SUM(`bet_money` * `play_rebate`) AS `back_money` 
                   FROM `bet_his` WHERE 1 AND `testFlag` IN(0,2) AND `bet_his`.`status` = 1 ";
         $aArray = [];
