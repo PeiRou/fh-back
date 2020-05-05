ae324e8f8a844c530b63a616861cc7752b51923e
zoe <zoe@gmail.com>
2020-05-05 15:14:45 +0800

    optimization lottery

--- app/Console/Commands/BUNKO/BUNKO_1.php
+++ app/Console/Commands/BUNKO/BUNKO_1.php
@@ -52,6 +52,7 @@ class BUNKO_1 extends Command
             if($update)
                 $excel->all($opennum,$get->issue,$get->id,false,$code,$lotterys);
         }else{
+            //如果现在没有需要开奖的，把下一期时间放到缓存里，减少读取次数
             $excel->setNextBunkoIssue($lotterys['table'],$code);
         }
     }
