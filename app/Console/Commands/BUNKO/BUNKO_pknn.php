d6e1ebc6150daec2a3c5800bb5977642f6123805
zoe <zoe@gmail.com>
2020-05-06 15:46:49 +0800

    modify pknn not exe bunkp

--- app/Console/Commands/BUNKO/BUNKO_pknn.php
+++ app/Console/Commands/BUNKO/BUNKO_pknn.php
@@ -26,16 +26,22 @@ class BUNKO_pknn extends Command
         if(empty($lotterys))
             return false;
         $excel = new New_pknn();
+        //阻止彩种進行中
+        if($excel->stopBunko($code, 1,'BunkoCP'))
+            return 'ing';
         $get = $excel->getNeedBunkoIssue($lotterys['table']);
         if($get){
             //阻止進行中
-            if($excel->stopBunko($lotterys['gameId'], 10,'Bunko:'.$get->issue));
+            if($excel->stopBunko($lotterys['gameId'], 10,'Bunko:'.$get->issue))
                 return 'ing';
             $update = DB::table($lotterys['table'])->where('id', $get->id)->update([
                 'bunko' => 2
             ]);
             if($update)
                 $excel->all($get->opennum,$get->niuniu, $get->issue, $get->id,$code,$lotterys); //新--结算
+        }else{
+            //如果现在没有需要开奖的，把下一期时间放到缓存里，减少读取次数
+            $excel->setNextBunkoIssue($lotterys['table'],$code);
         }
     }
 }
