58ce712bf8a22b26bcb763400b88db27ba0106bf
zoe <zoe@gmail.com>
2020-05-06 16:40:14 +0800

    注释 特定休市彩种-删奖期

--- app/Console/Commands/clear_data.php
+++ app/Console/Commands/clear_data.php
@@ -218,14 +218,14 @@ class clear_data extends Command
                 }
                 $num_else = $this->clrGameTables($val['table'], $newClearDate, $num_else);
             }
-            //特定休市彩种-删奖期
-            $res = ['game_bjpk10','game_pcdd','game_bjkl8','game_pknn'];
-            foreach ($res as $key => $table){
-                $num_else = $this->delGameTables($table, '2020-01-22 00:00:00', '2020-06-30 23:59:59', $num_else);
-            }
+            //特定休市彩种-删奖期----留给未来使用
+//            $res = ['game_bjpk10','game_pcdd','game_bjkl8','game_pknn'];
+//            foreach ($res as $key => $table){
+//                $num_else = $this->delGameTables($table, '2020-01-22 00:00:00', '2020-06-30 23:59:59', $num_else);
+//            }
             //特定休市彩种-封盘
-            $resGameIds = [50,66,65,90];
-            DB::table('game')->whereIn('game_id',$resGameIds)->update(['holiday_start'=>'2020-01-22 00:00:00','holiday_end'=>'2020-06-30 23:59:59']);
+//            $resGameIds = [50,66,65,90];
+//            DB::table('game')->whereIn('game_id',$resGameIds)->update(['holiday_start'=>'2020-01-22 00:00:00','holiday_end'=>'2020-06-30 23:59:59']);
             //清-计画试算
             $num_else = $this->clrGameTables('plan_record', $clearDate2, $num_else,'updated_at');
             //清-推送消息
